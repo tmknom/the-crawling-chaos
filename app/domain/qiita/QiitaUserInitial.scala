754755de9b2609ddb47aa26ebe7cfffcb752a9f3
tmknom <tmk.nom@gmail.com>
2017-11-25 19:40:19 +0900

    QiitaUserInitialにpageRangeメソッド追加

--- app/domain/qiita/QiitaUserInitial.scala
+++ app/domain/qiita/QiitaUserInitial.scala
@@ -1,7 +1,13 @@
 package domain.qiita
 
-final case class QiitaUserInitial(initial: Initial, page: Page)
+final case class QiitaUserInitial(initial: Initial, page: Page) {
+  def pageRange: Range = page.range
+}
 
 final case class Initial(value: Char)
 
-final case class Page(value: Int)
+final case class Page(value: Int) {
+  def range: Range = {
+    1 to value
+  }
+}
