e1fd775f589e537aceb9c0df34596721dd231c3a
tmknom <tmk.nom@gmail.com>
2017-11-25 20:30:57 +0900

    QiitaUserIdの追加

--- app/domain/qiita/user/QiitaUser.scala
+++ app/domain/qiita/user/QiitaUser.scala
@@ -1,5 +1,7 @@
 package domain.qiita.user
 
-final case class QiitaUser()
+final case class QiitaUser(id: QiitaUserId, name: QiitaUserName)
+
+final case class QiitaUserId(value: Int)
 
 final case class QiitaUserName(value: String)
