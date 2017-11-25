d37ee30f595cabcfa0baf3676f27e5022d901123
tmknom <tmk.nom@gmail.com>
2017-11-26 01:07:15 +0900

    QiitaUserRankingGatewayのfetchメソッドを実装

--- app/infrastructure/qiita/userranking/HttpQiitaUserRankingGateway.scala
+++ app/infrastructure/qiita/userranking/HttpQiitaUserRankingGateway.scala
@@ -1,9 +1,12 @@
 package infrastructure.qiita.userranking
 
 import domain.qiita.userranking.{QiitaUserRanking, QiitaUserRankingGateway}
+import library.adaptor.DispatchAdaptor
 
 final class HttpQiitaUserRankingGateway extends QiitaUserRankingGateway {
   override def fetch(page: Int): Seq[QiitaUserRanking] = {
-    Seq.empty[QiitaUserRanking]
+    val url      = s"https://qiita-user-ranking.herokuapp.com/?page=${page.toString}"
+    val response = DispatchAdaptor(url).request()
+    QiitaUserRankingParser(response).parse
   }
 }
---
+++ app/infrastructure/qiita/userranking/QiitaUserRankingParser.scala
@@ -0,0 +1,29 @@
+package infrastructure.qiita.userranking
+
+import domain.qiita.userranking.{
+  QiitaUserRanking,
+  QiitaUserRankingContribution,
+  QiitaUserRankingId,
+  QiitaUserRankingName
+}
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.Element
+
+private[userranking] final case class QiitaUserRankingParser(html: String) {
+  def parse: Seq[QiitaUserRanking] = {
+    val SkipElement = 4
+    val doc         = JsoupBrowser().parseString(html)
+    val items       = doc >> elementList("main p")
+    items.drop(SkipElement).map(qiitaUserRanking)
+  }
+
+  private def qiitaUserRanking(item: Element): QiitaUserRanking = {
+    QiitaUserRanking(
+      id           = QiitaUserRankingId(-1),
+      name         = QiitaUserRankingName(item >> element("a") >> text),
+      contribution = QiitaUserRankingContribution(item.innerHtml.split(", ").last.toInt)
+    )
+  }
+}
