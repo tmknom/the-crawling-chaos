6c1a2d016cc8f9d75e006f23bc7d5b74c17fc361
tmknom <tmk.nom@gmail.com>
2017-11-26 01:11:09 +0900

    ScalikejdbcQiitaUserRankingRepositoryのregisterメソッド実装

--- app/infrastructure/qiita/userranking/ScalikejdbcQiitaUserRankingRepository.scala
+++ app/infrastructure/qiita/userranking/ScalikejdbcQiitaUserRankingRepository.scala
@@ -4,5 +4,9 @@ import domain.qiita.userranking.{QiitaUserRanking, QiitaUserRankingRepository}
 import scalikejdbc._
 
 final class ScalikejdbcQiitaUserRankingRepository extends QiitaUserRankingRepository {
-  override def register(qiitaUserRanking: QiitaUserRanking)(implicit session: DBSession = AutoSession): Unit = {}
+  override def register(qiitaUserRanking: QiitaUserRanking)(implicit session: DBSession = AutoSession): Unit = {
+    val userName     = qiitaUserRanking.name.value
+    val contribution = qiitaUserRanking.contribution.value
+    sql"INSERT INTO qiita_user_rankings (user_name, contribution) VALUES ($userName, $contribution);".update.apply()
+  }
 }
