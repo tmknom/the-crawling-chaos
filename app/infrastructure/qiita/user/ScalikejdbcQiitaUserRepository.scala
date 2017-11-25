e7996ed1374958bbf2d202b90149f4b23a43001e
tmknom <tmk.nom@gmail.com>
2017-11-25 20:43:39 +0900

    ScalikejdbcQiitaUserRepositoryのregisterメソッドを実装

--- app/infrastructure/qiita/user/ScalikejdbcQiitaUserRepository.scala
+++ app/infrastructure/qiita/user/ScalikejdbcQiitaUserRepository.scala
@@ -4,5 +4,8 @@ import domain.qiita.user.{QiitaUser, QiitaUserRepository}
 import scalikejdbc._
 
 final class ScalikejdbcQiitaUserRepository extends QiitaUserRepository {
-  override def register(qiitaUser: QiitaUser)(implicit session: DBSession = AutoSession): Unit = {}
+  override def register(qiitaUser: QiitaUser)(implicit session: DBSession = AutoSession): Unit = {
+    val userName = qiitaUser.name.value
+    sql"INSERT INTO qiita_users (user_name) VALUES ($userName);".update.apply()
+  }
 }
