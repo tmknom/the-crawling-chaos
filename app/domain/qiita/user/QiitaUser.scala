7d15d08bc00ac9c044f7cbad85c804103cff6565
tmknom <tmk.nom@gmail.com>
2017-11-26 13:07:03 +0900

    QiitaUserIdクラスを別ファイルに切り出し

--- app/domain/qiita/user/QiitaUser.scala
+++ app/domain/qiita/user/QiitaUser.scala
@@ -2,6 +2,4 @@ package domain.qiita.user
 
 final case class QiitaUser(id: QiitaUserId, name: QiitaUserName)
 
-final case class QiitaUserId(value: Int)
-
 final case class QiitaUserName(value: String)
