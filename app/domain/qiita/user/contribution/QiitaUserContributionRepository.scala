8a3a56bb0b080161e0711ca5d4f266eafc692f14
tmknom <tmk.nom@gmail.com>
2017-11-26 14:15:49 +0900

    QiitaUserContributionRepositoryにregisterメソッド実装

--- app/domain/qiita/user/contribution/QiitaUserContributionRepository.scala
+++ app/domain/qiita/user/contribution/QiitaUserContributionRepository.scala
@@ -1,6 +1,9 @@
 package domain.qiita.user.contribution
 
+import domain.qiita.user.QiitaUserId
 import scalikejdbc.{AutoSession, DBSession}
 
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments"))
-trait QiitaUserContributionRepository {}
+trait QiitaUserContributionRepository {
+  def register(qiitaUserId: QiitaUserId, qiitaUserContribution: QiitaUserContribution)(implicit session: DBSession = AutoSession): Unit
+}
