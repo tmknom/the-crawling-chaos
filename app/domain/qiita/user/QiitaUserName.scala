1f96bd45a7a25e169d11ea33f9e3511ac716e198
tmknom <tmk.nom@gmail.com>
2017-11-26 13:19:32 +0900

    QiitaUserNameにurlHovercardUsersメソッドを定義

--- app/domain/qiita/user/QiitaUserName.scala
+++ app/domain/qiita/user/QiitaUserName.scala
@@ -1,3 +1,7 @@
 package domain.qiita.user
 
-final case class QiitaUserName(value: String)
+final case class QiitaUserName(value: String) {
+  def urlHovercardUsers: String = {
+    s"https://qiita.com/api/internal/hovercard_users/$value"
+  }
+}
--- app/domain/qiita/userranking/QiitaUserRanking.scala
+++ app/domain/qiita/userranking/QiitaUserRanking.scala
@@ -2,6 +2,8 @@ package domain.qiita.userranking
 
 import domain.qiita.user.{QiitaUserId, QiitaUserName}
 
-final case class QiitaUserRanking(qiitaUserId: QiitaUserId, name: QiitaUserName, contribution: QiitaUserRankingContribution)
+final case class QiitaUserRanking(qiitaUserId: QiitaUserId, name: QiitaUserName, contribution: QiitaUserRankingContribution) {
+  def urlHovercardUsers: String = name.urlHovercardUsers
+}
 
 final case class QiitaUserRankingContribution(value: Int)
