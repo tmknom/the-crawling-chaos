7059114b2df12e5c4d8f63ac8dd425ca88b16b30
tmknom <tmk.nom@gmail.com>
2017-11-26 12:55:08 +0900

    QiitaUserRankingRepositoryにretrieveAllメソッドを実装

--- app/domain/qiita/userranking/QiitaUserRankingRepository.scala
+++ app/domain/qiita/userranking/QiitaUserRankingRepository.scala
@@ -5,4 +5,6 @@ import scalikejdbc.{AutoSession, DBSession}
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments"))
 trait QiitaUserRankingRepository {
   def register(qiitaUserRanking: QiitaUserRanking)(implicit session: DBSession = AutoSession): Unit
+
+  def retrieveAll()(implicit session: DBSession = AutoSession): Seq[QiitaUserRanking]
 }
--- app/infrastructure/qiita/userranking/ScalikejdbcQiitaUserRankingRepository.scala
+++ app/infrastructure/qiita/userranking/ScalikejdbcQiitaUserRankingRepository.scala
@@ -1,6 +1,6 @@
 package infrastructure.qiita.userranking
 
-import domain.qiita.userranking.{QiitaUserRanking, QiitaUserRankingRepository}
+import domain.qiita.userranking._
 import scalikejdbc._
 
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments", "org.wartremover.warts.Nothing"))
@@ -11,5 +11,12 @@ final class ScalikejdbcQiitaUserRankingRepository extends QiitaUserRankingReposi
     sql"INSERT INTO qiita_user_rankings (user_name, contribution) VALUES ($userName, $contribution);".update.apply()
 
     () // 明示的に Unit を返す
+  }
+
+  def retrieveAll()(implicit session: DBSession = AutoSession): Seq[QiitaUserRanking] = {
+    sql"SELECT qu.id, qur.user_name, qur.contribution FROM qiita_user_rankings AS qur INNER JOIN qiita_users AS qu ON qur.user_name = qu.user_name ORDER BY qur.contribution DESC LIMIT 10;".map {
+      rs =>
+        QiitaUserRanking(QiitaUserRankingId(rs.int("id")), QiitaUserRankingName(rs.string("user_name")), QiitaUserRankingContribution(rs.int("contribution")))
+    }.list().apply()
   }
 }
