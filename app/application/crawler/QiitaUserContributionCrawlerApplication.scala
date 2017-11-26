dd3c0acac48271f3c6fed6855a2caf4b098fc26e
tmknom <tmk.nom@gmail.com>
2017-11-26 16:21:00 +0900

    QiitaUserContributionCrawlerApplicationでエラーになったユーザ名を最後にログ出力する

--- app/application/crawler/QiitaUserContributionCrawlerApplication.scala
+++ app/application/crawler/QiitaUserContributionCrawlerApplication.scala
@@ -7,6 +7,8 @@ import domain.qiita.user.contribution.{QiitaUserContributionGateway, QiitaUserCo
 import domain.qiita.user.ranking.{QiitaUserRanking, QiitaUserRankingRepository}
 import play.api.Logger
 
+import scala.collection.mutable
+
 @Singleton
 final class QiitaUserContributionCrawlerApplication @Inject()(
     gateway:                    QiitaUserContributionGateway,
@@ -16,6 +18,9 @@ final class QiitaUserContributionCrawlerApplication @Inject()(
 
   private val SLEEP_TIME_MILLISECONDS = 250.toLong
 
+  @SuppressWarnings(Array("org.wartremover.warts.MutableDataStructures"))
+  private val errorQiitaUserNames = mutable.ListBuffer.empty[String]
+
   def crawl(): Unit = {
     val qiitaUserRankings: Seq[QiitaUserRanking] = qiitaUserRankingRepository.retrieveAll()
     qiitaUserRankings.zipWithIndex.foreach {
@@ -23,6 +28,7 @@ final class QiitaUserContributionCrawlerApplication @Inject()(
         quietlyCrawlOneUser(qiitaUserRanking, index)
         TimeUnit.MILLISECONDS.sleep(SLEEP_TIME_MILLISECONDS)
     }
+    Logger.info(s"crawl error ${errorQiitaUserNames.size.toString} users ( ${errorQiitaUserNames.mkString(",")} )")
   }
 
   private def quietlyCrawlOneUser(qiitaUserRanking: QiitaUserRanking, index: Int): Unit = {
@@ -30,13 +36,16 @@ final class QiitaUserContributionCrawlerApplication @Inject()(
       crawlOneUser(qiitaUserRanking, index)
     } catch {
       // 処理を止めてほしくないのでログ吐いて握りつぶす
-      case e: Exception => Logger.warn(s"crawl error ${index.toString} ${qiitaUserRanking.name.value}.", e)
+      case e: Exception => {
+        errorQiitaUserNames += qiitaUserRanking.name.value
+        Logger.warn(s"crawl error ${index.toString} ${qiitaUserRanking.name.value}.", e)
+      }
     }
   }
 
   private def crawlOneUser(qiitaUserRanking: QiitaUserRanking, index: Int): Unit = {
     val qiitaUserContribution = gateway.fetch(qiitaUserRanking.name)
     repository.register(qiitaUserRanking.qiitaUserId, qiitaUserContribution)
-    Logger.info(s"crawled ${index + 1} : ${qiitaUserRanking.name} : ${qiitaUserContribution.value}")
+    Logger.info(s"crawled ${index + 1} : ${qiitaUserRanking.name} : $qiitaUserContribution")
   }
 }
