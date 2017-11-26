4b7d2eb81febabf12618b0ef68177da23c0fe97c
tmknom <tmk.nom@gmail.com>
2017-11-26 13:32:27 +0900

    QiitaUserRankingからurlHovercardUsersメソッド削除

--- app/domain/qiita/userranking/QiitaUserRanking.scala
+++ app/domain/qiita/userranking/QiitaUserRanking.scala
@@ -2,8 +2,6 @@ package domain.qiita.userranking
 
 import domain.qiita.user.{QiitaUserId, QiitaUserName}
 
-final case class QiitaUserRanking(qiitaUserId: QiitaUserId, name: QiitaUserName, contribution: QiitaUserRankingContribution) {
-  def urlHovercardUsers: String = name.urlHovercardUsers
-}
+final case class QiitaUserRanking(qiitaUserId: QiitaUserId, name: QiitaUserName, contribution: QiitaUserRankingContribution)
 
 final case class QiitaUserRankingContribution(value: Int)
