df24ebae6f56234e4dca907da22acdf239fa609e
tmknom <tmk.nom@gmail.com>
2017-11-29 18:09:04 +0900

    HttpQiitaUserContributionGatewayがScalajHttpAdaptorを使うよう修正

--- app/infrastructure/qiita/user/contribution/HttpQiitaUserContributionGateway.scala
+++ app/infrastructure/qiita/user/contribution/HttpQiitaUserContributionGateway.scala
@@ -2,13 +2,12 @@ package infrastructure.qiita.user.contribution
 
 import domain.qiita.user.QiitaUserName
 import domain.qiita.user.contribution.{QiitaUserContribution, QiitaUserContributionGateway}
-
-import scalaj.http.Http
+import library.scalaj.ScalajHttpAdaptor
 
 final class HttpQiitaUserContributionGateway extends QiitaUserContributionGateway {
 
   def fetch(qiitaUserName: QiitaUserName): QiitaUserContribution = {
-    val response: String = Http(qiitaUserName.urlHovercardUsers).asString.throwError.body
+    val response = ScalajHttpAdaptor.get(qiitaUserName.urlHovercardUsers)
     QiitaUserContributionParser(response).parse
   }
 }
