ec7783858526f793b2c3cf20c763cf0033f4ef01
tmknom <tmk.nom@gmail.com>
2017-12-02 13:20:37 +0900

    データベースフィクスチャ含めてロールバックするよう修正

--- test/infrastructure/qiita/user/ScalikejdbcQiitaUserRepositorySpec.scala
+++ test/infrastructure/qiita/user/ScalikejdbcQiitaUserRepositorySpec.scala
@@ -1,11 +1,10 @@
 package infrastructure.qiita.user
 
-import com.ninja_squad.dbsetup.destination.DataSourceDestination
-import com.ninja_squad.dbsetup.{DbSetup, Operations}
+import com.ninja_squad.dbsetup.Operations
+import com.ninja_squad.dbsetup.bind.DefaultBinderConfiguration
 import domain.qiita.user.{QiitaUser, QiitaUserId, QiitaUserName}
 import library.test.db.DatabaseSpec
 import org.scalatest.OptionValues
-import play.api.db.DBApi
 
 class ScalikejdbcQiitaUserRepositorySpec extends DatabaseSpec with OptionValues {
   "ScalikejdbcQiitaUserRepository#register" should {
@@ -23,8 +22,8 @@ class ScalikejdbcQiitaUserRepositorySpec extends DatabaseSpec with OptionValues 
 
   "ScalikejdbcQiitaUserRepository#retrieveAll" should {
     "一覧できること" in { implicit session =>
-      val dataSource  = app.injector.instanceOf[DBApi].database("default").dataSource
-      val destination = new DataSourceDestination(dataSource)
+      val connection = session.connection
+
       val operations = Operations.sequenceOf(
         Operations
           .insertInto("qiita_users")
@@ -34,10 +33,11 @@ class ScalikejdbcQiitaUserRepositorySpec extends DatabaseSpec with OptionValues 
           .values("3", "kira")
           .build()
       )
-      val dbSetup = new DbSetup(destination, operations)
-      dbSetup.launch()
 
-      val sut    = new ScalikejdbcQiitaUserRepository()
+      connection.setAutoCommit(false)
+      operations.execute(connection, DefaultBinderConfiguration.INSTANCE)
+
+      val sut = new ScalikejdbcQiitaUserRepository()
       val actual = sut.retrieveAll()
 
       actual.size mustBe 3
