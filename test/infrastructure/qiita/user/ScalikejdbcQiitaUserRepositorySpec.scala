11006c6c4f775613ce510c0e5658ff636c9ba1f2
tmknom <tmk.nom@gmail.com>
2017-12-02 12:16:54 +0900

    ScalikejdbcQiitaUserRepositoryのretrieveAllメソッドのテスト追加

--- test/infrastructure/qiita/user/ScalikejdbcQiitaUserRepositorySpec.scala
+++ test/infrastructure/qiita/user/ScalikejdbcQiitaUserRepositorySpec.scala
@@ -1,8 +1,11 @@
 package infrastructure.qiita.user
 
+import com.ninja_squad.dbsetup.destination.DataSourceDestination
+import com.ninja_squad.dbsetup.{DbSetup, Operations}
 import domain.qiita.user.{QiitaUser, QiitaUserId, QiitaUserName}
 import library.test.db.DatabaseSpec
 import org.scalatest.OptionValues
+import play.api.db.DBApi
 
 class ScalikejdbcQiitaUserRepositorySpec extends DatabaseSpec with OptionValues {
   "ScalikejdbcQiitaUserRepository#register" should {
@@ -14,6 +17,30 @@ class ScalikejdbcQiitaUserRepositorySpec extends DatabaseSpec with OptionValues 
 
       val actual = sut.retrieveAll()
       actual.size mustBe 1
+      actual.headOption.value.name mustBe QiitaUserName("jojo")
+    }
+  }
+
+  "ScalikejdbcQiitaUserRepository#retrieveAll" should {
+    "一覧できること" in { implicit session =>
+      val dataSource  = app.injector.instanceOf[DBApi].database("default").dataSource
+      val destination = new DataSourceDestination(dataSource)
+      val operations = Operations.sequenceOf(
+        Operations
+          .insertInto("qiita_users")
+          .columns("id", "user_name")
+          .values("1", "jojo")
+          .values("2", "dio")
+          .values("3", "kira")
+          .build()
+      )
+      val dbSetup = new DbSetup(destination, operations)
+      dbSetup.launch()
+
+      val sut    = new ScalikejdbcQiitaUserRepository()
+      val actual = sut.retrieveAll()
+
+      actual.size mustBe 3
       actual.headOption.value.name mustBe QiitaUserName("jojo")
     }
   }
