e0d45d12d8d2dc7cbe814493c013783d6e5f95dd
tmknom <tmk.nom@gmail.com>
2017-12-02 14:22:25 +0900

    データベースフィクスチャを定義するFixtureDefinitionを作成

---
+++ test/library/test/db/FixtureDefinition.scala
@@ -0,0 +1,28 @@
+package library.test.db
+
+import com.ninja_squad.dbsetup.Operations
+import com.ninja_squad.dbsetup.bind.DefaultBinderConfiguration
+import com.ninja_squad.dbsetup.operation.Operation
+import scalikejdbc.DBSession
+
+/**
+  * データベースフィクスチャの定義
+  *
+  * 通常、DbSetup を使う場合は DbSetup#launch() メソッドを呼び出すが、
+  * コイツは内部的にコミット処理まで行っており AutoRollback によるロールバックが無効化される。
+  *
+  * そのため DbSetup#launch() メソッドから、コミット処理部分を取り除いたメソッドを独自実装し、
+  * データベースフィクスチャの定義は本メソッドを介して行うこととする。
+  *
+  * @see https://qiita.com/yo1000/items/a7c0825035b4fbcfa3b2
+  */
+object FixtureDefinition {
+  @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter"))
+  def define(operations: Operation*)(implicit session: DBSession): Unit = {
+    val connection = session.connection
+    connection.setAutoCommit(false)
+
+    val operation = Operations.sequenceOf(operations: _*)
+    operation.execute(connection, DefaultBinderConfiguration.INSTANCE)
+  }
+}
