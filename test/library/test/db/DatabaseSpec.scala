fc9a648286d565616e0a892c2c3a7371aa08f7ec
tmknom <tmk.nom@gmail.com>
2017-12-02 16:40:04 +0900

    DatabaseSpecがBasicSpecAggregationをextendsするよう修正

--- test/library/test/db/DatabaseSpec.scala
+++ test/library/test/db/DatabaseSpec.scala
@@ -1,10 +1,11 @@
 package library.test.db
 
+import library.test.BasicSpecAggregation
 import library.test.db.internal.PreparationDatabaseSpec
-import org.scalatest.{fixture, MustMatchers}
+import org.scalatest.fixture
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
 /**
   * データベーステスト基底トレイト
   */
-trait DatabaseSpec extends fixture.WordSpec with MustMatchers with GuiceOneAppPerSuite with PreparationDatabaseSpec
+trait DatabaseSpec extends fixture.WordSpec with BasicSpecAggregation with GuiceOneAppPerSuite with PreparationDatabaseSpec
