d57ee9e7babc70efe30aa7d1fde72aee28695df6
tmknom <tmk.nom@gmail.com>
2017-12-05 00:11:23 +0900

    QiitaUserSummaryResponseの実装

--- app/presentation/controller/QiitaUserContributionController.scala
+++ app/presentation/controller/QiitaUserContributionController.scala
@@ -3,11 +3,30 @@ package presentation.controller
 import javax.inject.Inject
 
 import application.qiita.user.QiitaUserContributionApplication
+import domain.qiita.user.summary.QiitaUserSummary
+import library.spray.SnakeCaseJsonNaming
 import play.api.mvc._
+import spray.json._
 
 class QiitaUserContributionController @Inject()(cc: ControllerComponents, application: QiitaUserContributionApplication) extends AbstractController(cc) {
   def list(): Action[AnyContent] = Action {
-    val reuslt = application.list()
-    Ok("ok")
+    import QiitaUserSummaryResponse._
+    val result = application.list().map(toResponse)
+    Ok(result.toJson.prettyPrint).as(JSON)
   }
+}
+
+object QiitaUserSummaryResponse extends SnakeCaseJsonNaming {
+
+  final case class QiitaUserSummaryResponse(id: Int, name: String, contribution: Int)
+
+  def toResponse(qiitaUserSummary: QiitaUserSummary): QiitaUserSummaryResponse = {
+    QiitaUserSummaryResponse(
+      qiitaUserSummary.id.value,
+      qiitaUserSummary.name.value,
+      qiitaUserSummary.contribution.value
+    )
+  }
+
+  implicit val format: RootJsonFormat[QiitaUserSummaryResponse] = jsonFormat3(QiitaUserSummaryResponse)
 }
