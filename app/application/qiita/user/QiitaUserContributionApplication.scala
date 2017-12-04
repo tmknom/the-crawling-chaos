78ede719250ccefb9323e5198d82a4f601d3963a
tmknom <tmk.nom@gmail.com>
2017-12-04 21:20:35 +0900

    QiitaUserContributionApplicationの実装

---
+++ app/application/qiita/user/QiitaUserContributionApplication.scala
@@ -0,0 +1,14 @@
+package application.qiita.user
+
+import javax.inject.{Inject, Singleton}
+
+import domain.qiita.user.summary.{QiitaUserSummary, QiitaUserSummaryRepository}
+
+@Singleton
+final class QiitaUserContributionApplication @Inject()(
+    repository: QiitaUserSummaryRepository
+) {
+  def list(): List[QiitaUserSummary] = {
+    repository.retrieveTop200()
+  }
+}
