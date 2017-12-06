8648ea5df3363886524f95db3c1ef443f92e87d2
tmknom <tmk.nom@gmail.com>
2017-12-06 09:27:51 +0900

    FixtureDefinitionをDatabaseFixtureにリネーム

--- test/library/test/DatabaseFixture.scala
+++ test/library/test/DatabaseFixture.scala
@@ -6,17 +6,17 @@ import com.ninja_squad.dbsetup.operation.Operation
 import scalikejdbc.DBSession
 
 /**
-  * データベースフィクスチャの定義
+  * データベースフィクスチャのセットアップ
   *
   * 通常、DbSetup を使う場合は DbSetup#launch() メソッドを呼び出すが、
   * コイツは内部的にコミット処理まで行っており AutoRollback によるロールバックが無効化される。
   *
   * そのため DbSetup#launch() メソッドから、コミット処理部分を取り除いたメソッドを独自実装し、
-  * データベースフィクスチャの定義は本メソッドを介して行うこととする。
+  * データベースフィクスチャのセットアップは本メソッドを介して行うこととする。
   *
   * @see https://qiita.com/yo1000/items/a7c0825035b4fbcfa3b2
   */
-object FixtureDefinition {
+object DatabaseFixture {
   @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter"))
   def define(operations: Operation*)(implicit session: DBSession): Unit = {
     val connection = session.connection
