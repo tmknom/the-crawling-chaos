04d9e2533a1031c4129c8ba2a481183c4235a9d4
tmknom <tmk.nom@gmail.com>
2018-01-04 15:51:23 +0900

    qiita_user_contribution_historiesテーブルのマイグレーション定義

---
+++ conf/db/migration/default/V20180104_154400__create_qiita_user_contribution_histories_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS qiita_user_contribution_histories (
+  qiita_user_id        INT(11)     NOT NULL,
+  contribution         INT(11)     NOT NULL COMMENT 'いいね数',
+  registered_date      DATE        NOT NULL COMMENT '登録日',
+  registered_date_time DATETIME(6) NOT NULL COMMENT '登録日時',
+  UNIQUE (qiita_user_id, registered_date),
+  FOREIGN KEY (qiita_user_id) REFERENCES qiita_users (id)
+) ENGINE InnoDB DEFAULT CHARSET utf8mb4 DEFAULT COLLATE utf8mb4_bin COMMENT 'Qiitaユーザいいね数履歴';
