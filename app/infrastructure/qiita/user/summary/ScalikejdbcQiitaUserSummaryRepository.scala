21467c84c24f40c28645f27936d0128ce0bcc894
tmknom <tmk.nom@gmail.com>
2018-01-04 22:03:29 +0900

    ScalikejdbcQiitaUserSummaryRepositoryのSQLを改行

--- app/infrastructure/qiita/user/summary/ScalikejdbcQiitaUserSummaryRepository.scala
+++ app/infrastructure/qiita/user/summary/ScalikejdbcQiitaUserSummaryRepository.scala
@@ -11,15 +11,25 @@ import scalikejdbc._
 @Singleton
 final class ScalikejdbcQiitaUserSummaryRepository extends QiitaUserSummaryRepository {
   def retrieveTop200()(implicit session: DBSession = AutoSession): List[QiitaUserSummary] = {
-    // scalastyle:off
-    sql"SELECT qu.id, qu.user_name, quc.contribution, quc.articles_count FROM qiita_user_contributions AS quc INNER JOIN qiita_users AS qu ON quc.qiita_user_id = qu.id ORDER BY quc.contribution DESC LIMIT 20;".map {
-      rs =>
-        QiitaUserSummary(
-          QiitaUserId(rs.int("id")),
-          QiitaUserName(rs.string("user_name")),
-          QiitaUserContribution(rs.int("contribution")),
-          ArticlesCount(rs.int("articles_count"))
-        )
-    }.list().apply()
+    sql"""
+          SELECT qu.id, qu.user_name, quc.contribution, quc.articles_count
+          FROM qiita_user_contributions AS quc
+          INNER JOIN qiita_users AS qu
+          ON quc.qiita_user_id = qu.id
+          ORDER BY quc.contribution DESC
+          LIMIT 200;
+       """
+      .map(toQiitaUserSummary)
+      .list()
+      .apply()
+  }
+
+  private def toQiitaUserSummary(rs: WrappedResultSet): QiitaUserSummary = {
+    QiitaUserSummary(
+      QiitaUserId(rs.int("id")),
+      QiitaUserName(rs.string("user_name")),
+      QiitaUserContribution(rs.int("contribution")),
+      ArticlesCount(rs.int("articles_count"))
+    )
   }
 }
