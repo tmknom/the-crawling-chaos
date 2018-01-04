dce308776b2caf2cd1184c3e8b4526747d5052a8
tmknom <tmk.nom@gmail.com>
2018-01-04 16:35:27 +0900

    qiita_user_contribution_historiesテーブルにarticles_countカラム追加

--- conf/db/migration/default/V20180104_154400__create_qiita_user_contribution_histories_table.sql
+++ conf/db/migration/default/V20180104_154400__create_qiita_user_contribution_histories_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS qiita_user_contribution_histories (
   qiita_user_id        INT(11)     NOT NULL,
   contribution         INT(11)     NOT NULL COMMENT 'いいね数',
+  articles_count       INT(11)     NOT NULL COMMENT '記事数',
   registered_date      DATE        NOT NULL COMMENT '登録日',
   registered_date_time DATETIME(6) NOT NULL COMMENT '登録日時',
   UNIQUE (qiita_user_id, registered_date),
