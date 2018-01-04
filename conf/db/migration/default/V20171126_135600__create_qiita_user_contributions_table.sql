b39acd6db134e3efbd76f6b54cc546226707b729
tmknom <tmk.nom@gmail.com>
2018-01-04 15:14:37 +0900

    qiita_user_contributionsテーブルにupdated_date_timeカラムを追加

--- conf/db/migration/default/V20171126_135600__create_qiita_user_contributions_table.sql
+++ conf/db/migration/default/V20171126_135600__create_qiita_user_contributions_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS qiita_user_contributions (
-  qiita_user_id INT(11) NOT NULL,
-  contribution  INT(11) NOT NULL COMMENT 'いいね数',
+  qiita_user_id     INT(11)     NOT NULL,
+  contribution      INT(11)     NOT NULL COMMENT 'いいね数',
+  updated_date_time DATETIME(6) NOT NULL COMMENT '更新日時',
   PRIMARY KEY (qiita_user_id),
   FOREIGN KEY (qiita_user_id) REFERENCES qiita_users (id)
 ) ENGINE InnoDB DEFAULT CHARSET utf8mb4 DEFAULT COLLATE utf8mb4_bin COMMENT 'Qiitaユーザいいね数';
