51ef6a62c95846a7f858cdf6d563b385d112d7a2
tmknom <tmk.nom@gmail.com>
2018-01-05 23:18:31 +0900

    QiitaArticleListCrawlerApplicationが例外が発生しても処理を継続するよう変更

--- app/application/crawler/QiitaArticleListCrawlerApplication.scala
+++ app/application/crawler/QiitaArticleListCrawlerApplication.scala
@@ -4,30 +4,40 @@ import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 
 import domain.qiita.article.page.QiitaArticlePage
-import domain.qiita.article.{QiitaArticleGateway, QiitaArticleListGateway, QiitaArticleRepository}
+import domain.qiita.article.{QiitaArticleIdRepository, QiitaArticleListGateway, QiitaItemId}
 import play.api.Logger
 
 @Singleton
 final class QiitaArticleListCrawlerApplication @Inject()(
     qiitaArticleListGateway: QiitaArticleListGateway,
-    qiitaArticleGateway:     QiitaArticleGateway,
-    repository:              QiitaArticleRepository
+    repository:              QiitaArticleIdRepository
 ) {
   private val SleepTimeMilliseconds = 100.toLong
 
   def crawl(): Unit = {
     QiitaArticlePage.range.foreach { currentPage =>
-      val articles = qiitaArticleListGateway.fetch(currentPage)
-
-      articles.foreach { article =>
-        val (postedDateTime, body) = qiitaArticleGateway.fetch(article.itemId)
-        repository.register(article)
-        TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
-      }
-
-      Logger.info(s"crawled $currentPage / ${QiitaArticlePage.PageMax}")
+      quietlyCrawl(currentPage)
       TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
     }
   }
 
+  private def quietlyCrawl(currentPage: Int): Unit = {
+    try {
+      val articles = qiitaArticleListGateway.fetch(currentPage)
+      articles.foreach(article => quietlyRegister(article.itemId))
+      Logger.info(s"crawled $currentPage / ${QiitaArticlePage.PageMax}")
+    } catch {
+      case e: Exception =>
+        Logger.warn(s"${this.getClass.getSimpleName} crawl error ${currentPage.toString}.", e)
+    }
+  }
+
+  private def quietlyRegister(qiitaItemId: QiitaItemId): Unit = {
+    try {
+      repository.register(qiitaItemId)
+    } catch {
+      case e: Exception =>
+        Logger.warn(s"${this.getClass.getSimpleName} register error ${qiitaItemId.value}.", e)
+    }
+  }
 }
