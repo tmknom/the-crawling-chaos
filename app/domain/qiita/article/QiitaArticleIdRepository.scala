6f2bb9286885879ec648b23904bd5a11507944a5
tmknom <tmk.nom@gmail.com>
2018-01-06 01:47:37 +0900

    QiitaArticleIdRepositoryにretrieveAllメソッド追加

--- app/domain/qiita/article/QiitaArticleIdRepository.scala
+++ app/domain/qiita/article/QiitaArticleIdRepository.scala
@@ -5,4 +5,6 @@ import scalikejdbc.{AutoSession, DBSession}
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments"))
 trait QiitaArticleIdRepository {
   def register(qiitaItemId: QiitaItemId)(implicit session: DBSession = AutoSession): Unit
+
+  def retrieveAll()(implicit session: DBSession = AutoSession): List[QiitaItemId]
 }
