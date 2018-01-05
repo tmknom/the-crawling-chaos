79fd122b2bf78271fceb9f1ee58bce978ac26943
tmknom <tmk.nom@gmail.com>
2018-01-05 22:40:01 +0900

    QiitaArticleListCrawlerApplicationで記事取得APIを叩くよう修正

--- app/application/crawler/QiitaArticleListCrawlerApplication.scala
+++ app/application/crawler/QiitaArticleListCrawlerApplication.scala
@@ -4,21 +4,25 @@ import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 
 import domain.qiita.article.page.QiitaArticlePage
-import domain.qiita.article.{QiitaArticleListGateway, QiitaArticleRepository}
+import domain.qiita.article.{QiitaArticleGateway, QiitaArticleListGateway, QiitaArticleRepository}
 import play.api.Logger
 
 @Singleton
 final class QiitaArticleListCrawlerApplication @Inject()(
-    gateway:    QiitaArticleListGateway,
-    repository: QiitaArticleRepository
+    qiitaArticleListGateway: QiitaArticleListGateway,
+    qiitaArticleGateway:     QiitaArticleGateway,
+    repository:              QiitaArticleRepository
 ) {
   private val SleepTimeMilliseconds = 100.toLong
 
   def crawl(): Unit = {
     QiitaArticlePage.range.foreach { currentPage =>
-      val articles = gateway.fetch(currentPage)
+      val articles = qiitaArticleListGateway.fetch(currentPage)
+
       articles.foreach { article =>
+        val (postedDateTime, body) = qiitaArticleGateway.fetch(article.itemId)
         repository.register(article)
+        TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
       }
 
       Logger.info(s"crawled $currentPage / ${QiitaArticlePage.PageMax}")
