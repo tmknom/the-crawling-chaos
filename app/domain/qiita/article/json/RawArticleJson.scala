fbe98e19f91b32031a3e1b4057b471b51f7ba06d
tmknom <tmk.nom@gmail.com>
2018-01-06 14:10:03 +0900

    RawArticleJsonにtoLikesCountメソッドとtoCommentsCountメソッドを定義

--- app/domain/qiita/article/json/RawArticleJson.scala
+++ app/domain/qiita/article/json/RawArticleJson.scala
@@ -1,9 +1,26 @@
 package domain.qiita.article.json
 
-import spray.json.{JsValue, JsonParser}
+import domain.qiita.article.contribution.{CommentsCount, LikesCount}
+import spray.json.DefaultJsonProtocol._
+import spray.json._
 
 final case class RawArticleJson(value: String) {
   def toJsonMap: Map[String, JsValue] = {
     JsonParser(value).asJsObject.fields
+  }
+
+  def toLikesCount: LikesCount = {
+    LikesCount(parseJson[Int](toJsonMap, "likesCount"))
+  }
+
+  def toCommentsCount: CommentsCount = {
+    CommentsCount(parseJson[Int](toJsonMap, "commentsCount"))
+  }
+
+  private def parseJson[T: JsonReader](jsonMap: Map[String, JsValue], keyName: String): T = {
+    jsonMap.get(keyName) match {
+      case Some(value) => value.convertTo[T]
+      case None        => throw new RuntimeException(s"Not found $keyName.")
+    }
   }
 }
