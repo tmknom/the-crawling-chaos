b4b179365e907de2bab8c2cb3fee52246d8efe66
tmknom <tmk.nom@gmail.com>
2018-01-06 14:31:12 +0900

    QiitaArticleRepositoryにretrieveメソッド実装

--- app/infrastructure/qiita/article/ScalikejdbcQiitaArticleRepository.scala
+++ app/infrastructure/qiita/article/ScalikejdbcQiitaArticleRepository.scala
@@ -2,7 +2,8 @@ package infrastructure.qiita.article
 
 import javax.inject.Singleton
 
-import domain.qiita.article.{QiitaArticle, QiitaArticleRepository}
+import domain.qiita.article._
+import domain.qiita.user.QiitaUserName
 import scalikejdbc._
 
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments", "org.wartremover.warts.Nothing"))
@@ -23,5 +24,34 @@ final class ScalikejdbcQiitaArticleRepository extends QiitaArticleRepository {
       .apply()
 
     () // 明示的に Unit を返す
+  }
+
+  override def retrieve(qiitaItemId: QiitaItemId)(implicit session: DBSession = AutoSession): QiitaArticle = {
+    val itemId = qiitaItemId.value
+
+    val optionValue =
+      sql"""
+          SELECT * FROM qiita_articles
+          WHERE item_id = $itemId;
+       """
+        .map(toQiitaArticle)
+        .single()
+        .apply()
+
+    optionValue match {
+      case Some(v) => v
+      case None    => throw new RuntimeException(s"Not exists QiitaItemId: $itemId")
+    }
+  }
+
+  private def toQiitaArticle(rs: WrappedResultSet): QiitaArticle = {
+    QiitaArticle(
+      QiitaArticleId(rs.int("id")),
+      QiitaItemId(rs.string("item_id")),
+      QiitaArticleTitle(rs.string("title")),
+      QiitaArticleUrl(rs.string("url")),
+      QiitaArticlePostedDateTime(rs.zonedDateTime("posted_date_time")),
+      QiitaUserName(rs.string("posted_user_name"))
+    )
   }
 }
