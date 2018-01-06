3d91ac23115458e2b22184ed4d13cca93f662a6c
tmknom <tmk.nom@gmail.com>
2018-01-06 13:14:58 +0900

    QiitaArticleContributionCrawlerCliの実装

---
+++ app/presentation/cli/operation/crawler/article/QiitaArticleContributionCrawlerCli.scala
@@ -0,0 +1,22 @@
+package presentation.cli.operation.crawler.article
+
+import application.crawler.QiitaArticleContributionCrawlerApplication
+import library.task.Task
+import play.api.{Application, Logger}
+
+/**
+  * run-main presentation.cli.operation.crawler.article.QiitaArticleContributionCrawlerCli
+  */
+object QiitaArticleContributionCrawlerCli extends App with Task {
+  run()
+
+  override def task(app: Application): Unit = {
+    try {
+      Logger.info(s"Started ${this.getClass.getSimpleName}.")
+      app.injector.instanceOf[QiitaArticleContributionCrawlerApplication].crawl()
+      Logger.info(s"Completed ${this.getClass.getSimpleName}.")
+    } catch {
+      case e: Exception => Logger.error(s"Failed ${this.getClass.getSimpleName}.", e)
+    }
+  }
+}
