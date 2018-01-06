6d49a12482b3858e819d6eb7585dfaf2dc71ad47
tmknom <tmk.nom@gmail.com>
2018-01-06 13:50:22 +0900

    QiitaRawArticleJsonRepositoryにretrieveAllメソッド追加

--- app/infrastructure/qiita/article/json/ScalikejdbcQiitaRawArticleJsonRepository.scala
+++ app/infrastructure/qiita/article/json/ScalikejdbcQiitaRawArticleJsonRepository.scala
@@ -2,7 +2,7 @@ package infrastructure.qiita.article.json
 
 import javax.inject.Singleton
 
-import domain.qiita.article.QiitaItemId
+import domain.qiita.article._
 import domain.qiita.article.json.{QiitaRawArticleJsonRepository, RawArticleJson}
 import scalikejdbc._
 
@@ -20,5 +20,22 @@ final class ScalikejdbcQiitaRawArticleJsonRepository extends QiitaRawArticleJson
       .apply()
 
     () // 明示的に Unit を返す
+  }
+
+  override def retrieveAll()(implicit session: DBSession = AutoSession): List[(QiitaItemId, RawArticleJson)] = {
+    sql"""
+          SELECT item_id, raw_json FROM raw_qiita_article_jsons AS rqaj
+          INNER JOIN qiita_articles AS qa ON rqaj.qiita_article_item_id = qa.item_id
+          WHERE NOT EXISTS
+          (SELECT 1 FROM qiita_article_contributions AS qac WHERE rqaj.qiita_article_item_id = qa.item_id)
+          ORDER BY id ASC;
+       """
+      .map(map)
+      .list()
+      .apply()
+  }
+
+  private def map(rs: WrappedResultSet): (QiitaItemId, RawArticleJson) = {
+    (QiitaItemId(rs.string("item_id")), RawArticleJson(rs.string("raw_json")))
   }
 }
