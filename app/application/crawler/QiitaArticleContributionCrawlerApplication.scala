cc8340d012cd5da3b83aad42a82dcc337e0b9f09
tmknom <tmk.nom@gmail.com>
2018-01-06 15:54:33 +0900

    QiitaArticleContributionCrawlerApplicationの実装

--- app/application/crawler/QiitaArticleContributionCrawlerApplication.scala
+++ app/application/crawler/QiitaArticleContributionCrawlerApplication.scala
@@ -3,38 +3,54 @@ package application.crawler
 import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 
-import domain.qiita.article.json.QiitaRawArticleJsonRepository
-import domain.qiita.article.{QiitaArticleGateway, QiitaArticleRepository, QiitaItemId}
+import domain.qiita.article.contribution.{QiitaArticleContributionRepository, _}
+import domain.qiita.article.json.{QiitaRawArticleJsonRepository, RawArticleJson}
+import domain.qiita.article.{QiitaArticleRepository, QiitaItemId}
 import play.api.Logger
 
 @Singleton
 final class QiitaArticleContributionCrawlerApplication @Inject()(
-    gateway:           QiitaArticleGateway,
-    repository:        QiitaArticleRepository,
-    rawJsonRepository: QiitaRawArticleJsonRepository
+    repository:             QiitaArticleContributionRepository,
+    hatenaGateway:          HatenaGateway,
+    facebookGateway:        FacebookGateway,
+    qiitaArticleRepository: QiitaArticleRepository,
+    rawJsonRepository:      QiitaRawArticleJsonRepository
 ) {
   private val SleepTimeMilliseconds = 100.toLong
 
   def crawl(): Unit = {
-    val qiitaItemIds = List.empty[QiitaItemId]
-    qiitaItemIds.zipWithIndex.foreach {
-      case (qiitaItemId, index) =>
-        quietlyCrawl(qiitaItemId, index, qiitaItemIds.size)
+    val items = rawJsonRepository.retrieveAll()
+    items.zipWithIndex.foreach {
+      case ((qiitaItemId, rawArticleJson), index) =>
+        quietlyCrawl(qiitaItemId, rawArticleJson, index, items.size)
         TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
     }
   }
 
-  private def quietlyCrawl(qiitaItemId: QiitaItemId, index: Int, qiitaItemIdsCount: Int): Unit = {
+  private def quietlyCrawl(qiitaItemId: QiitaItemId, rawArticleJson: RawArticleJson, index: Int, itemsCount: Int): Unit = {
     try {
-      log(qiitaItemId, index, qiitaItemIdsCount)
+      val qiitaArticle    = qiitaArticleRepository.retrieve(qiitaItemId)
+      val qiitaArticleUrl = qiitaArticle.url
+
+      val hatenaCount   = hatenaGateway.fetch(qiitaArticleUrl)
+      val facebookCount = facebookGateway.fetch(qiitaArticleUrl)
+
+      val qiitaArticleContribution = QiitaArticleContribution(
+        rawArticleJson.toLikesCount,
+        rawArticleJson.toCommentsCount,
+        hatenaCount,
+        facebookCount
+      )
+      repository.register(qiitaArticle.id, qiitaArticleContribution)
+      log(qiitaItemId, index, itemsCount)
     } catch {
       case e: Exception =>
         Logger.warn(s"${this.getClass.getSimpleName} crawl error ${qiitaItemId.value}.", e)
     }
   }
 
-  private def log(qiitaItemId: QiitaItemId, index: Int, qiitaItemIdsCount: Int) = {
-    val progress = ((index + 1) / qiitaItemIdsCount) * 100.0
-    Logger.info(s"${this.getClass.getSimpleName} crawled ${qiitaItemId.value} : ${index + 1} / $qiitaItemIdsCount ($progress%)")
+  private def log(qiitaItemId: QiitaItemId, index: Int, itemsCount: Int) = {
+    val progress = ((index + 1) / itemsCount) * 100.0
+    Logger.info(s"${this.getClass.getSimpleName} crawled ${qiitaItemId.value} : ${index + 1} / $itemsCount ($progress%)")
   }
 }
