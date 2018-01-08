37a4072f8c24826b747938cba61303ff78b37d2a
tmknom <tmk.nom@gmail.com>
2018-01-08 13:03:50 +0900

    QiitaArticleIdCrawlerApplicationが登録済みの情報を登録しようとしたら正常終了する

--- app/application/crawler/QiitaArticleIdCrawlerApplication.scala
+++ app/application/crawler/QiitaArticleIdCrawlerApplication.scala
@@ -3,6 +3,7 @@ package application.crawler
 import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException
 import domain.qiita.article.page.QiitaArticlePage
 import domain.qiita.article.{QiitaArticleIdGateway, QiitaArticleIdRepository}
 import play.api.Logger
@@ -15,20 +16,23 @@ final class QiitaArticleIdCrawlerApplication @Inject()(
   private val SleepTimeMilliseconds = 100.toLong
 
   def crawl(): Unit = {
-    QiitaArticlePage.range.foreach { currentPage =>
-      quietlyCrawl(currentPage)
-      TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
+    try {
+      QiitaArticlePage.range.foreach { currentPage =>
+        crawlOnePage(currentPage)
+        TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
+      }
+    } catch {
+      // すでに登録済みの情報を登録しようとしたら、MySQLIntegrityConstraintViolationExceptionが飛ぶ
+      // あまり行儀がよくないが、重複エラーが処理終了のタイイングと合致して都合がいいので、
+      // 例外を飛ばしてそいつをキャッチして終了することにした
+      case e: MySQLIntegrityConstraintViolationException =>
+        Logger.info(s"${this.getClass.getSimpleName} crawled end because ${e.getMessage}")
     }
   }
 
-  private def quietlyCrawl(currentPage: Int): Unit = {
-    try {
-      val qiitaItemIds = gateway.fetch(currentPage)
-      qiitaItemIds.foreach(repository.register)
-      Logger.info(s"crawled $currentPage / ${QiitaArticlePage.PageMax}")
-    } catch {
-      case e: Exception =>
-        Logger.warn(s"${this.getClass.getSimpleName} crawl error ${currentPage.toString}.", e)
-    }
+  private def crawlOnePage(currentPage: Int): Unit = {
+    val qiitaItemIds = gateway.fetch(currentPage)
+    qiitaItemIds.foreach(repository.register)
+    Logger.info(s"crawled $currentPage / ${QiitaArticlePage.PageMax}")
   }
 }
