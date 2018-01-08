cf2a6575dd7c74777440c0098faff98e1d387857
tmknom <tmk.nom@gmail.com>
2018-01-08 12:57:49 +0900

    QiitaArticleIdCrawlerApplicationのリファクタリング

--- app/application/crawler/QiitaArticleIdCrawlerApplication.scala
+++ app/application/crawler/QiitaArticleIdCrawlerApplication.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 
 import domain.qiita.article.page.QiitaArticlePage
-import domain.qiita.article.{QiitaArticleIdGateway, QiitaArticleIdRepository, QiitaItemId}
+import domain.qiita.article.{QiitaArticleIdGateway, QiitaArticleIdRepository}
 import play.api.Logger
 
 @Singleton
@@ -24,22 +24,11 @@ final class QiitaArticleIdCrawlerApplication @Inject()(
   private def quietlyCrawl(currentPage: Int): Unit = {
     try {
       val qiitaItemIds = gateway.fetch(currentPage)
-      qiitaItemIds.foreach { qiitaItemId =>
-        quietlyRegister(qiitaItemId)
-      }
+      qiitaItemIds.foreach(repository.register)
       Logger.info(s"crawled $currentPage / ${QiitaArticlePage.PageMax}")
     } catch {
       case e: Exception =>
         Logger.warn(s"${this.getClass.getSimpleName} crawl error ${currentPage.toString}.", e)
-    }
-  }
-
-  private def quietlyRegister(qiitaItemId: QiitaItemId): Unit = {
-    try {
-      repository.register(qiitaItemId)
-    } catch {
-      case e: Exception =>
-        Logger.warn(s"${this.getClass.getSimpleName} register error ${qiitaItemId.value}.", e)
     }
   }
 }
