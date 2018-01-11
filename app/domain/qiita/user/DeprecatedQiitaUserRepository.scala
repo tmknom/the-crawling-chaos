7925ec344a6fd85ca059b20d4552385a714cb342
tmknom <tmk.nom@gmail.com>
2018-01-11 23:58:46 +0900

    未使用Cliの削除

--- app/domain/qiita/user/DeprecatedQiitaUserRepository.scala
+++ app/domain/qiita/user/DeprecatedQiitaUserRepository.scala
@@ -4,13 +4,7 @@ import scalikejdbc.{AutoSession, DBSession}
 
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments"))
 trait DeprecatedQiitaUserRepository {
-  def delete(qiitaUserId: QiitaUserId)(implicit session: DBSession = AutoSession): Unit
-
-  def retrieveRecently()(implicit session: DBSession = AutoSession): Seq[DeprecatedQiitaUser]
-
   def retrieveContributed()(implicit session: DBSession = AutoSession): Seq[DeprecatedQiitaUser]
 
   def retrieveTop1000()(implicit session: DBSession = AutoSession): Seq[DeprecatedQiitaUser]
-
-  def retrieveUnavailable()(implicit session: DBSession = AutoSession): Seq[DeprecatedQiitaUser]
 }
