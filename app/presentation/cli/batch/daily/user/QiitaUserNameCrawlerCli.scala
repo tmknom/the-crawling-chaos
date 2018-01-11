2326bd418bcda3bd874a86e8dbdadc7d42d705a4
tmknom <tmk.nom@gmail.com>
2018-01-11 09:34:08 +0900

    RecentlyQiitaUserNameCrawlerApplicationをQiitaUserNameCrawlerApplicationにリネーム

--- app/presentation/cli/batch/daily/user/QiitaUserNameCrawlerCli.scala
+++ app/presentation/cli/batch/daily/user/QiitaUserNameCrawlerCli.scala
@@ -1,6 +1,6 @@
 package presentation.cli.batch.daily.user
 
-import application.crawler.user.RecentlyQiitaUserNameCrawlerApplication
+import application.crawler.user.QiitaUserNameCrawlerApplication
 import library.task.Task
 import play.api.{Application, Logger}
 
@@ -13,7 +13,7 @@ object QiitaUserNameCrawlerCli extends App with Task {
   override def task(app: Application): Unit = {
     try {
       Logger.info(s"Started ${this.getClass.getSimpleName}.")
-      app.injector.instanceOf[RecentlyQiitaUserNameCrawlerApplication].crawl()
+      app.injector.instanceOf[QiitaUserNameCrawlerApplication].crawl()
       Logger.info(s"Completed ${this.getClass.getSimpleName}.")
     } catch {
       case e: Exception => Logger.error(s"Failed ${this.getClass.getSimpleName}.", e)
