38161b3480f091327dabcd49fb0b4c4cdae9c58f
tmknom <tmk.nom@gmail.com>
2018-01-12 09:38:38 +0900

    QiitaUserRepositoryにretrieveメソッド追加

--- app/domain/qiita/user/QiitaUserRepository.scala
+++ app/domain/qiita/user/QiitaUserRepository.scala
@@ -5,4 +5,6 @@ import scalikejdbc.{AutoSession, DBSession}
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments"))
 trait QiitaUserRepository {
   def register(qiitaUser: QiitaUser)(implicit session: DBSession = AutoSession): Unit
+
+  def retrieve(qiitaUserName: QiitaUserName)(implicit session: DBSession = AutoSession): Option[QiitaUser]
 }
