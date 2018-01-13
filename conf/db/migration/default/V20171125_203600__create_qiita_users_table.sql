5cb4a06c0dca5d7ff27a31c9c0e435a9db237716
tmknom <tmk.nom@gmail.com>
2018-01-13 14:00:52 +0900

    deprecated_qiita_usersテーブルの削除

--- conf/db/migration/default/V20171125_203600__create_qiita_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS deprecated_qiita_users (
-  id                   INT(11)      NOT NULL AUTO_INCREMENT,
-  user_name            VARCHAR(255) NOT NULL COMMENT 'ユーザ名',
-  registered_date_time DATETIME(6)  NOT NULL COMMENT '登録日時',
-  PRIMARY KEY (id),
-  UNIQUE (user_name)
-) ENGINE InnoDB AUTO_INCREMENT 1 DEFAULT CHARSET utf8mb4 DEFAULT COLLATE utf8mb4_bin COMMENT 'Qiitaユーザ';
