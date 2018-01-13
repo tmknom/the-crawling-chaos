cf6a3dd3883fa24a53fca32a44b8ab22c38142dd
tmknom <tmk.nom@gmail.com>
2018-01-13 16:57:25 +0900

    データのエクスポートタスクを実装

--- fabfile.py
+++ fabfile.py
@@ -10,6 +10,32 @@ def init_db():
     load_data()
 
 
+@task
+def export_data():
+    '''データのエクスポート'''
+    local('sudo rm -rf /tmp/*.csv')
+    export_data_table('qiita_user_names', 'user_name ASC')
+    export_data_table('raw_qiita_internal_user_jsons', 'crawled_date_time ASC')
+    export_data_table('qiita_users', 'qiita_user_id ASC')
+    export_data_table('qiita_user_contributions', 'updated_date_time ASC')
+    export_data_table('qiita_user_contribution_histories', 'registered_date_time ASC')
+    local('ls -alh /tmp/*.csv')
+    local('wc -l /tmp/*.csv')
+
+
+def export_data_table(table_name, order):
+    execute_sql(export_data_sql(table_name, order))
+
+
+def export_data_sql(table_name, order):
+    return ' SELECT * FROM %s ' % table_name \
+           + ' ORDER BY %s ' % order \
+           + ' INTO OUTFILE \'/tmp/%s.csv\' ' % table_name \
+           + ' FIELDS TERMINATED BY \'\t\' ' \
+           + ' ENCLOSED BY \'\\"\' ' \
+           + ' ESCAPED BY \'\\"\' ; '
+
+
 def clean_migrate():
     '''クリーンマイグレーション'''
     command = "sbt flywayClean flywayMigrate"
