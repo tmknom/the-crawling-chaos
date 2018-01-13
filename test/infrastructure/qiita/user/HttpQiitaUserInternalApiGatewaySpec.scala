efaa39e387df2e8f178072dace80834af3c59d7e
tmknom <tmk.nom@gmail.com>
2018-01-13 10:38:38 +0900

    HttpQiitaUserInternalApiGatewayのテスト追加

---
+++ test/infrastructure/qiita/user/HttpQiitaUserInternalApiGatewaySpec.scala
@@ -0,0 +1,41 @@
+package infrastructure.qiita.user
+
+import domain.qiita.user._
+import library.scalaj.{HttpHeaders, HttpParams, HttpTimeout, ScalajHttpAdaptor}
+import org.scalatestplus.play.PlaySpec
+
+// scalastyle:off magic.number
+class HttpQiitaUserInternalApiGatewaySpec extends PlaySpec {
+  val sut = new HttpQiitaUserInternalApiGateway(new MockScalajHttpAdaptor())
+
+  "#fetch" should {
+    "通信できること" in {
+      val actual = sut.fetch(QiitaUserName("jojo"))
+
+      val expected = QiitaUser(
+        QiitaUserId(123456789),
+        QiitaUserName("jojo"),
+        ProfileImageUrl("https://qiita-image-store.s3.amazonaws.com/xxxx")
+      )
+
+      actual.toQiitaUser mustBe expected
+    }
+  }
+
+  class MockScalajHttpAdaptor extends ScalajHttpAdaptor {
+    def get(url: String, httpParams: HttpParams, headers: HttpHeaders, timeout: HttpTimeout): String = {
+      """
+        |{
+        |  "articles_count":98,
+        |  "contribution":1234,
+        |  "followed":false,
+        |  "id":123456789,
+        |  "name":"",
+        |  "profile_image_url":"https://qiita-image-store.s3.amazonaws.com/xxxx",
+        |  "url_name":"jojo"
+        |}
+      """.stripMargin
+    }
+  }
+
+}
