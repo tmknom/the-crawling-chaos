1eb41a66eaba2fc5ea652374358878c0c7649297
tmknom <tmk.nom@gmail.com>
2018-01-13 19:44:26 +0900

    CSVのダウンロードタスクを実装

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,19 @@ TABLE_NAMES = [
 
 
 @task
+def download_csv():
+    '''-H $SSH_HOST -u $SSH_USER_NAME --port=$SSH_PORT CSVのダウンロード'''
+    for table_name in TABLE_NAMES:
+        download_csv_table(table_name)
+
+
+def download_csv_table(table_name):
+    csv_path = get_local_env('CSV_PATH')
+    print('%s.csv downloading...' % table_name)
+    get('/tmp/%s.csv' % table_name, '%s/%s.csv' % (csv_path, table_name))
+
+
+@task
 def export_data():
     '''データのエクスポート'''
     local('sudo rm -rf /tmp/*.csv')
