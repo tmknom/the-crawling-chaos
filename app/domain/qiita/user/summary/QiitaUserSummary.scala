2b1e244b3cc5b12b55af1c6c888efc163e9c3214
tmknom <tmk.nom@gmail.com>
2018-01-13 13:06:58 +0900

    QiitaUserSummaryの削除

--- app/domain/qiita/user/summary/QiitaUserSummary.scala
+++
@@ -1,6 +0,0 @@
-package domain.qiita.user.summary
-
-import domain.qiita.user.contribution.{ArticlesCount, QiitaUserContribution}
-import domain.qiita.user.{QiitaUserId, QiitaUserName}
-
-final case class QiitaUserSummary(id: QiitaUserId, name: QiitaUserName, contribution: QiitaUserContribution, articlesCount: ArticlesCount)
