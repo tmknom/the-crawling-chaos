eb0fb6df1f11a33086bd7ece3fc09c2ac2de44df
tmknom <tmk.nom@gmail.com>
2018-01-13 19:50:40 +0900

    デプロイタスクを実装

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,16 @@ TABLE_NAMES = [
 
 
 @task
+def deploy():
+    '''デプロイ'''
+    local('git checkout develop')
+    local('git pull --rebase origin develop')
+    local('sbt dist')
+    local('rm -rf /tmp/qiita-ranker-1.0-SNAPSHOT')
+    local('unzip target/universal/qiita-ranker-1.0-SNAPSHOT.zip -d /tmp')
+
+
+@task
 def download_csv():
     '''-H $SSH_HOST -u $SSH_USER_NAME --port=$SSH_PORT CSVのダウンロード'''
     for table_name in TABLE_NAMES:
