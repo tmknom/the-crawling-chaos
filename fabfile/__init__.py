1b07bf0c1d127df9bf097a218ea7e4a583a018b6
tmknom <tmk.nom@gmail.com>
2018-01-13 23:42:26 +0900

    init_dbタスクを切り出し

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -56,41 +56,5 @@ def export_data_sql(table_name, order):
 @task
 def init_db():
     '''データベースの初期化'''
-    clean_migrate()
-    load_data()
-
-
-def clean_migrate():
-    '''クリーンマイグレーション'''
-    command = "sbt flywayClean flywayMigrate"
-    local(command)
-
-
-def load_data():
-    '''初期データのロード'''
-    for table_name in TABLE_NAMES:
-        load_data_table(table_name)
-
-
-def load_data_table(table_name):
-    execute_sql(load_data_sql(table_name))
-    execute_sql(select_count_sql(table_name))
-    print('\n')
-
-
-def load_data_sql(table_name):
-    csv_path = get_local_env('CSV_PATH')
-    return ' LOAD DATA INFILE \'%s/%s.csv\' ' % (csv_path, table_name) \
-           + ' INTO TABLE %s ' % table_name \
-           + ' FIELDS TERMINATED BY \'\t\' ' \
-           + ' ENCLOSED BY \'\\"\' ' \
-           + ' ESCAPED BY \'\\"\' ; '
-
-
-def select_count_sql(table_name):
-    return 'select count(*) from %s' % table_name
-
-
-def execute_sql(sql):
-    command = 'mysql -uroot db_production -e "%s"' % (sql)
-    local(command)
+    import init_db
+    init_db.execute()
