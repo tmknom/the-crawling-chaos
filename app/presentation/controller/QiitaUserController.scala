89d5618bb76c5523993c2e64873eafd5d0eca2c0
tmknom <tmk.nom@gmail.com>
2018-01-13 12:47:57 +0900

    QiitaUserControllerがJSONを返すよう変更

--- app/presentation/controller/QiitaUserController.scala
+++ app/presentation/controller/QiitaUserController.scala
@@ -3,12 +3,25 @@ package presentation.controller
 import javax.inject.Inject
 
 import application.qiita.user.QiitaUserApplication
+import domain.qiita.user.QiitaUser
 import play.api.mvc._
+import presentation.controller.internal.MapJsonProtocol._
+import spray.json._
 
-// scalastyle:off
 class QiitaUserController @Inject()(cc: ControllerComponents, application: QiitaUserApplication) extends AbstractController(cc) {
   def list(): Action[AnyContent] = Action {
-    val result = application.list()
-    Ok(result.toString())
+    val result = application.list().map(QiitaUserJson.build)
+    Ok(result.toJson.prettyPrint).as(JSON)
+  }
+}
+
+object QiitaUserJson {
+  def build(qiitaUser: QiitaUser): Map[String, _] = {
+    Map(
+      "id" -> qiitaUser.profile.id.value,
+      "name" -> qiitaUser.profile.name.value,
+      "contribution" -> qiitaUser.contribution.value,
+      "articles_count" -> qiitaUser.articlesCount.value
+    )
   }
 }
