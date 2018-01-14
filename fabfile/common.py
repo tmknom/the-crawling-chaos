53e3d04e01cb84ebaf75fd2e2875417b65f3c4ec
tmknom <tmk.nom@gmail.com>
2018-01-14 23:36:53 +0900

    bzip2ファイルをダウンロードするよう修正

--- fabfile/common.py
+++ fabfile/common.py
@@ -2,6 +2,8 @@
 
 from fabric.api import *
 
+TAR_NAME = 'csv.tar.bz2'
+
 
 def execute_sql(sql):
     command = 'mysql -uroot db_production -e "%s"' % (sql)
--- fabfile/export_data.py
+++ fabfile/export_data.py
@@ -5,20 +5,18 @@ from fabric.api import *
 import common
 from table import TABLES
 
-TAR_NAME = 'csv.tar.bz2'
-
 
 def execute():
     '''データのエクスポート'''
     local('sudo rm -rf /tmp/*.csv')
-    local('sudo rm -rf /tmp/%s' % TAR_NAME)
+    local('sudo rm -rf /tmp/%s' % common.TAR_NAME)
     for table in TABLES:
         export_data_table(table.name, table.order_query)
     with lcd('/tmp'):
-        local('tar cvjf %s *.csv' % TAR_NAME)
+        local('tar cvjf %s *.csv' % common.TAR_NAME)
     local('ls -alh /tmp/*.csv')
     local('wc -l /tmp/*.csv')
-    local('ls -lah /tmp/%s' % TAR_NAME)
+    local('ls -lah /tmp/%s' % common.TAR_NAME)
 
 
 def export_data_table(table_name, order):
