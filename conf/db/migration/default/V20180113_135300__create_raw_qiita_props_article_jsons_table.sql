f8e62806549ab2c9972e8a0eeeae150ded91c065
tmknom <tmk.nom@gmail.com>
2018-01-14 18:05:06 +0900

    raw_qiita_props_article_jsonsテーブルのマイグレーション定義

---
+++ conf/db/migration/default/V20180113_135300__create_raw_qiita_props_article_jsons_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS raw_qiita_props_article_jsons (
+  item_id           VARCHAR(255) NOT NULL COMMENT 'QiitaでのID',
+  raw_json          TEXT         NOT NULL COMMENT 'Json',
+  crawled_date_time DATETIME(6)  NOT NULL COMMENT 'クロールした日時',
+  PRIMARY KEY (item_id)
+) ENGINE InnoDB AUTO_INCREMENT 1 DEFAULT CHARSET utf8mb4 DEFAULT COLLATE utf8mb4_bin COMMENT 'QiitaのPropsArticleJson';
