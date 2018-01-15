bec236b9a4e835fc3269c1ecf2803604b6a258c8
tmknom <tmk.nom@gmail.com>
2018-01-15 22:55:46 +0900

    QiitaArticleRegisterApplicationの実装

---
+++ app/application/crawler/article/QiitaArticleRegisterApplication.scala
@@ -0,0 +1,52 @@
+package application.crawler.article
+
+import javax.inject.{Inject, Singleton}
+
+import domain.qiita.article.{QiitaArticleIdRepository, QiitaArticleRepository, QiitaItemId, QiitaRawPropsArticleJsonRepository}
+import play.api.Logger
+
+import scala.collection.mutable
+
+@Singleton
+final class QiitaArticleRegisterApplication @Inject()(
+    repository:        QiitaArticleRepository,
+    rawJsonRepository: QiitaRawPropsArticleJsonRepository,
+    idRepository:      QiitaArticleIdRepository
+) {
+
+  @SuppressWarnings(Array("org.wartremover.warts.MutableDataStructures"))
+  private val errors = mutable.ListBuffer.empty[String]
+
+  def crawl(): Unit = {
+    val qiitaItemIds = idRepository.retrieveNotRegistered()
+    val itemsCount   = qiitaItemIds.size
+    qiitaItemIds.zipWithIndex.foreach {
+      case (qiitaItemId, index) =>
+        val progress = calculateProgress(index, itemsCount)
+        quietlyRegister(qiitaItemId, progress)
+    }
+    Logger.warn(s"${this.getClass.getSimpleName} register error ${errors.size.toString} items : ${errors.toString()}")
+  }
+
+  private def quietlyRegister(qiitaItemId: QiitaItemId, progress: String): Unit = {
+    try {
+      val optionValue = rawJsonRepository.retrieve(qiitaItemId)
+      val rawArticleJson = optionValue match {
+        case Some(v) => v
+        case None    => throw new RuntimeException(s"ココに来たらバグなので雑に例外をスロー ${qiitaItemId.value}")
+      }
+
+      repository.register(rawArticleJson.toQiitaArticle)
+      Logger.info(s"${this.getClass.getSimpleName} registered ${qiitaItemId.value} $progress")
+    } catch {
+      case e: Exception =>
+        errors += qiitaItemId.value
+        Logger.warn(s"${this.getClass.getSimpleName} register error ${qiitaItemId.value}.", e)
+    }
+  }
+
+  private def calculateProgress(index: Int, itemsCount: Int): String = {
+    val progress = ((index + 1) / itemsCount.toDouble) * 100.0
+    s"(${index + 1} / $itemsCount = $progress%)"
+  }
+}
