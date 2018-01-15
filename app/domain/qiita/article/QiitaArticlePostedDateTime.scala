cd1136772181f0a18a285e65165774739978be5e
tmknom <tmk.nom@gmail.com>
2018-01-15 22:51:17 +0900

    QiitaArticlePostedDateTimeをUnixTimeから生成できるよう変更

--- app/domain/qiita/article/QiitaArticlePostedDateTime.scala
+++ app/domain/qiita/article/QiitaArticlePostedDateTime.scala
@@ -1,5 +1,15 @@
 package domain.qiita.article
 
-import java.time.ZonedDateTime
+import java.time.{Instant, ZonedDateTime}
+
+import library.datetime.DateTimeProvider
 
 final case class QiitaArticlePostedDateTime(value: ZonedDateTime)
+
+object QiitaArticlePostedDateTime {
+  def apply(unixTime: Int): QiitaArticlePostedDateTime = {
+    val instant       = Instant.ofEpochSecond(unixTime)
+    val zonedDateTime = ZonedDateTime.ofInstant(instant, DateTimeProvider.JST)
+    QiitaArticlePostedDateTime(zonedDateTime)
+  }
+}
