0f0e8ab8000b6c0a5c85dd0eecbe9e472a2a98de
tmknom <tmk.nom@gmail.com>
2018-01-17 22:33:14 +0900

    RawArticleJsonをリファクタリング

--- app/domain/qiita/article/json/RawArticleJson.scala
+++ app/domain/qiita/article/json/RawArticleJson.scala
@@ -35,17 +35,10 @@ final case class RawArticleJson(value: String) {
   }
 
   def toLikesCount: LikesCount = {
-    LikesCount(parseJson[Int](toJsonMap, "likesCount"))
+    LikesCount(parseJsonInt("likesCount"))
   }
 
   def toCommentsCount: CommentsCount = {
-    CommentsCount(parseJson[Int](toJsonMap, "commentsCount"))
-  }
-
-  private def parseJson[T: JsonReader](jsonMap: Map[String, JsValue], keyName: String): T = {
-    jsonMap.get(keyName) match {
-      case Some(value) => value.convertTo[T]
-      case None        => throw new RuntimeException(s"Not found $keyName.")
-    }
+    CommentsCount(parseJsonInt("commentsCount"))
   }
 }
