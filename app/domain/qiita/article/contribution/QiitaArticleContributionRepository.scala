bea6c1e8c0ba0389a7f1d5fa5cf96970854efc77
tmknom <tmk.nom@gmail.com>
2018-01-18 17:50:17 +0900

    QiitaArticleContributionRepositoryのregisterメソッドのシグネチャ変更

--- app/domain/qiita/article/contribution/QiitaArticleContributionRepository.scala
+++ app/domain/qiita/article/contribution/QiitaArticleContributionRepository.scala
@@ -1,9 +1,8 @@
 package domain.qiita.article.contribution
 
-import domain.qiita.article.QiitaItemId
 import scalikejdbc.{AutoSession, DBSession}
 
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.DefaultArguments"))
 trait QiitaArticleContributionRepository {
-  def register(qiitaItemId: QiitaItemId, qiitaArticleContribution: QiitaArticleContribution)(implicit session: DBSession = AutoSession): Unit
+  def register(event: QiitaArticleContributionCrawledEvent)(implicit session: DBSession = AutoSession): Unit
 }
