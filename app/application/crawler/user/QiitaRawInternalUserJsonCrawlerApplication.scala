70b630fb5bc6d18902fda113bbc891b53b8f1ed1
tmknom <tmk.nom@gmail.com>
2018-01-20 15:22:52 +0900

    QiitaRawInternalUserJsonCrawlerApplicationがBulkをextendsするよう修正

--- app/application/crawler/user/QiitaRawInternalUserJsonCrawlerApplication.scala
+++ app/application/crawler/user/QiitaRawInternalUserJsonCrawlerApplication.scala
@@ -2,34 +2,26 @@ package application.crawler.user
 
 import javax.inject.{Inject, Singleton}
 
-import domain.crawler.{Progress, QuietlyCrawler}
+import domain.crawler.{Bulk, QuietlyCrawler}
 import domain.qiita.user._
-import play.api.Logger
-
-import scala.collection.mutable
 
 @Singleton
 final class QiitaRawInternalUserJsonCrawlerApplication @Inject()(
     gateway:                 QiitaUserInternalApiGateway,
     repository:              QiitaRawInternalUserJsonRepository,
     qiitaUserNameRepository: QiitaUserNameRepository
-) extends QuietlyCrawler {
-  @SuppressWarnings(Array("org.wartremover.warts.MutableDataStructures"))
-  private val errorQiitaUserNames = mutable.ListBuffer.empty[String]
+) extends Bulk
+    with QuietlyCrawler {
 
   def crawl(): Unit = {
     val qiitaUserNames = qiitaUserNameRepository.retrieveRecently()
-    val itemsCount     = qiitaUserNames.size
-    qiitaUserNames.zipWithIndex.foreach {
-      case (qiitaUserName, index) =>
-        val progress = Progress.calculate(index, itemsCount)
-        quietlyCrawlOneUser(qiitaUserName, progress)
+    withLoop[QiitaUserName](qiitaUserNames) { (qiitaUserName, progress) =>
+      quietlyCrawlOneUser(qiitaUserName, progress)
     }
-    Logger.warn(s"${this.getClass.getSimpleName} crawl error ${errorQiitaUserNames.size.toString} users : ${errorQiitaUserNames.toString()}")
   }
 
   private def quietlyCrawlOneUser(qiitaUserName: QiitaUserName, progress: String): Unit = {
-    withQuietly[String](qiitaUserName, progress, errorQiitaUserNames) { (_) =>
+    withQuietly[String](qiitaUserName, progress, errors) { (_) =>
       val rawInternalUserJson = gateway.fetch(qiitaUserName)
       val crawledDateTime     = CrawledDateTime.now()
       repository.register(qiitaUserName, rawInternalUserJson, crawledDateTime)
