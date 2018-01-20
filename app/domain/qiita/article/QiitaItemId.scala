3e4d1d8157831427967b9df31a60dd9596243f38
tmknom <tmk.nom@gmail.com>
2018-01-20 13:32:40 +0900

    QiitaItemIdがIdentifierをextendsするよう変更

--- app/domain/qiita/article/QiitaItemId.scala
+++ app/domain/qiita/article/QiitaItemId.scala
@@ -1,6 +1,8 @@
 package domain.qiita.article
 
-final case class QiitaItemId(value: String) {
+import domain.Identifier
+
+final case class QiitaItemId(value: String) extends Identifier[String] {
   def apiUrl: String = {
     s"https://qiita.com/api/v2/items/$value"
   }
