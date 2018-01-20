69884dadd36ca74ffad910d71d2568095c64d22a
tmknom <tmk.nom@gmail.com>
2018-01-20 15:10:43 +0900

    QiitaRawPropsArticleJsonCrawlerApplicationがBulkをextendsするよう修正

--- app/application/crawler/article/QiitaRawPropsArticleJsonCrawlerApplication.scala
+++ app/application/crawler/article/QiitaRawPropsArticleJsonCrawlerApplication.scala
@@ -2,31 +2,23 @@ package application.crawler.article
 
 import javax.inject.{Inject, Singleton}
 
-import domain.crawler.{Progress, QuietlyCrawler}
+import domain.crawler.{Bulk, QuietlyCrawler}
 import domain.qiita.article._
 import domain.qiita.user.CrawledDateTime
-import play.api.Logger
-
-import scala.collection.mutable
 
 @Singleton
 final class QiitaRawPropsArticleJsonCrawlerApplication @Inject()(
     gateway:                  QiitaArticleInternalApiGateway,
     repository:               QiitaRawPropsArticleJsonRepository,
     qiitaArticleIdRepository: QiitaArticleIdRepository
-) extends QuietlyCrawler {
-  @SuppressWarnings(Array("org.wartremover.warts.MutableDataStructures"))
-  private val errors = mutable.ListBuffer.empty[String]
+) extends Bulk
+    with QuietlyCrawler {
 
   def crawl(): Unit = {
     val qiitaItemIds = qiitaArticleIdRepository.retrieveRecently()
-    val itemsCount   = qiitaItemIds.size
-    qiitaItemIds.zipWithIndex.foreach {
-      case (qiitaItemId, index) =>
-        val progress = Progress.calculate(index, itemsCount)
-        quietlyCrawl(qiitaItemId, progress)
+    withLoop[QiitaItemId](qiitaItemIds) { (qiitaItemId, progress) =>
+      quietlyCrawl(qiitaItemId, progress)
     }
-    Logger.warn(s"${this.getClass.getSimpleName} crawl error ${errors.size.toString} items : ${errors.toString()}")
   }
 
   private def quietlyCrawl(qiitaItemId: QiitaItemId, progress: String): Unit = {
