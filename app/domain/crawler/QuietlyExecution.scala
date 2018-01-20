7904d71cd904f621e8decc119342e0c404d5b63d
tmknom <tmk.nom@gmail.com>
2018-01-20 15:37:21 +0900

    QuietlyExecutionのログ出力のメッセージを調整

--- app/domain/crawler/QuietlyExecution.scala
+++ app/domain/crawler/QuietlyExecution.scala
@@ -9,11 +9,11 @@ trait QuietlyExecution {
   def withQuietly[T](identifier: Identifier[T], progress: String, errors: mutable.ListBuffer[String])(f: (Identifier[T]) => Unit): Unit = {
     try {
       f(identifier)
-      Logger.info(s"${this.getClass.getSimpleName} crawled ${identifier.value} $progress")
+      Logger.info(s"${this.getClass.getSimpleName} executed ${identifier.value} $progress")
     } catch {
       case e: Exception =>
         errors += identifier.value.toString
-        Logger.warn(s"${this.getClass.getSimpleName} crawl error ${identifier.value}.", e)
+        Logger.warn(s"${this.getClass.getSimpleName} executed error ${identifier.value}.", e)
     } finally {
       Sleeper.sleep()
     }
