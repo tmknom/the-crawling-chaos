5897160311b04126394d18a12cabfd0ef71a3f55
tmknom <tmk.nom@gmail.com>
2018-02-04 14:45:10 +0900

    HttpQiitaArticleApiGatewayがヘッダ付きでリクエストするよう修正

--- app/infrastructure/qiita/article/HttpQiitaArticleApiGateway.scala
+++ app/infrastructure/qiita/article/HttpQiitaArticleApiGateway.scala
@@ -4,12 +4,17 @@ import javax.inject.{Inject, Singleton}
 
 import domain.qiita.article.json.RawArticleJson
 import domain.qiita.article.{QiitaArticleApiGateway, QiitaItemId}
-import library.scalaj.ScalajHttpAdaptor
+import library.scalaj.{HttpHeaders, ScalajHttpAdaptor}
+import play.api.Configuration
 
 @Singleton
-final class HttpQiitaArticleApiGateway @Inject()(scalajHttpAdaptor: ScalajHttpAdaptor) extends QiitaArticleApiGateway {
+final class HttpQiitaArticleApiGateway @Inject()(scalajHttpAdaptor: ScalajHttpAdaptor, configuration: Configuration) extends QiitaArticleApiGateway {
+  private val AccessTokenKeyName = "app.qiita.access_token"
+
   override def fetch(qiitaItemId: QiitaItemId): RawArticleJson = {
-    val response = scalajHttpAdaptor.get(qiitaItemId.apiUrl)
+    val authorization: String = "Bearer " + configuration.get[String](AccessTokenKeyName)
+    val headers  = HttpHeaders(Map("Authorization" -> authorization))
+    val response = scalajHttpAdaptor.get(url = qiitaItemId.apiUrl, headers = headers)
     RawArticleJson(response)
   }
 }
