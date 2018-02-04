cafbde0379819598708701bd9be4d8e1f8820a4a
tmknom <tmk.nom@gmail.com>
2018-02-04 14:04:31 +0900

    QiitaArticleRegisterApplicationのcrawlメソッドをregisterにリネーム

--- app/presentation/cli/onetime/AllQiitaArticleRegisterCli.scala
+++ app/presentation/cli/onetime/AllQiitaArticleRegisterCli.scala
@@ -13,7 +13,7 @@ object AllQiitaArticleRegisterCli extends App with Task {
   override def task(app: Application): Unit = {
     try {
       Logger.info(s"Started ${this.getClass.getSimpleName}.")
-      app.injector.instanceOf[QiitaArticleRegisterApplication].crawl()
+      app.injector.instanceOf[QiitaArticleRegisterApplication].register()
       Logger.info(s"Completed ${this.getClass.getSimpleName}.")
     } catch {
       case e: Exception => Logger.error(s"Failed ${this.getClass.getSimpleName}.", e)
--- app/scenario/article/QiitaArticleCrawlerScenario.scala
+++ app/scenario/article/QiitaArticleCrawlerScenario.scala
@@ -18,7 +18,7 @@ final class QiitaArticleCrawlerScenario @Inject()(
     // Qiita記事のJSONをクロール
     qiitaRawPropsArticleJsonCrawlerApplication.crawl()
     // Qiita記事の情報情報を永続化
-    qiitaArticleRegisterApplication.crawl()
+    qiitaArticleRegisterApplication.register()
     // Qiita記事の評価を永続化
     qiitaArticleContributionCrawlerApplication.crawl()
   }
