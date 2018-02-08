3eebd1957594fea8730e840a871c4f8aaa9f1333
tmknom <tmk.nom@gmail.com>
2018-02-08 23:48:28 +0900

    element-uiのlocale設定を追加

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/ja'
 import App from './App.vue'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, {locale});
 
 new Vue({
   el: '#app',
