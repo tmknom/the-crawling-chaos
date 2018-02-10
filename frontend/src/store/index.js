be55b73c0ad81bffbd8dad546d1f07b9a42b30e4
tmknom <tmk.nom@gmail.com>
2018-02-10 14:26:39 +0900

    strictモードの有効化

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,5 +8,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     counter: counter
-  }
+  },
+  // https://vuex.vuejs.org/ja/strict.html
+  strict: process.env.NODE_ENV !== 'production'
 });
