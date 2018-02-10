3b7b06cd038adf81f6e1c32fc31bddcd27138db2
tmknom <tmk.nom@gmail.com>
2018-02-10 17:57:45 +0900

    Ajax通信部分をモデルに切り出し

--- frontend/src/store/modules/articles.js
+++ frontend/src/store/modules/articles.js
@@ -1,14 +1,26 @@
+import api from '../../api/articles';
+
 export default {
   namespaced: true,
   state: {
     items: []
   },
   mutations: {
+    fetchJson(state, payload) {
+      api.request(payload.jsonType).then((result) => {
+        state.items = result;
+      }).catch(function (error) {
+        console.log(error);
+      });
+    },
     replaceItems(state, payload) {
       state.items = payload.items
     }
   },
   actions: {
+    fetchJson({commit}, payload) {
+      commit('fetchJson', payload)
+    },
     replaceItems({commit}, payload) {
       commit('replaceItems', payload)
     }
