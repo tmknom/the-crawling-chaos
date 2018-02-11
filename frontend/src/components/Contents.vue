bcde1a8f186453e7ad94f3209ac9349d8b762327
tmknom <tmk.nom@gmail.com>
2018-02-12 01:27:49 +0900

    記事一覧にページネーション機能実装

--- frontend/src/components/Contents.vue
+++ frontend/src/components/Contents.vue
@@ -8,15 +8,47 @@
     <b-tabs type="is-boxed" @change="handle" expanded>
       <b-tab-item :label="tabs.contribution" icon-pack="fab" icon="qiita">
         <ArticleList :articles="items"></ArticleList>
+
+        <b-pagination
+          :total="pagination.total"
+          :per-page="pagination.perPage"
+          :order="pagination.order"
+          :current.sync="pagination.current.contribution"
+          @change="pageChange">
+        </b-pagination>
       </b-tab-item>
       <b-tab-item :label="tabs.hatena" icon-pack="fab" icon="hatena">
         <ArticleList :articles="items"></ArticleList>
+
+        <b-pagination
+          :total="pagination.total"
+          :per-page="pagination.perPage"
+          :order="pagination.order"
+          :current.sync="pagination.current.hatena"
+          @change="pageChange">
+        </b-pagination>
       </b-tab-item>
       <b-tab-item :label="tabs.facebook" icon-pack="fab" icon="facebook">
         <ArticleList :articles="items"></ArticleList>
+
+        <b-pagination
+          :total="pagination.total"
+          :per-page="pagination.perPage"
+          :order="pagination.order"
+          :current.sync="pagination.current.facebook"
+          @change="pageChange">
+        </b-pagination>
       </b-tab-item>
       <b-tab-item :label="tabs.pocket" icon-pack="fab" icon="get-pocket">
         <ArticleList :articles="items"></ArticleList>
+
+        <b-pagination
+          :total="pagination.total"
+          :per-page="pagination.perPage"
+          :order="pagination.order"
+          :current.sync="pagination.current.pocket"
+          @change="pageChange">
+        </b-pagination>
       </b-tab-item>
     </b-tabs>
   </section>
@@ -34,12 +66,23 @@
     },
     data() {
       return {
+        jsonType: 'contribution',
+        pagination: {
+          total: 10000,
+          perPage: 100,
+          order: 'is-centered',
+          current: {
+            'contribution': 1,
+            'hatena': 1,
+            'facebook': 1,
+            'pocket': 1,
+          },
+        },
         tabs: {
           'contribution': 'いいね！',
           'hatena': 'はてブ',
           'facebook': 'Facebook',
           'pocket': 'Pocket',
-
         }
       }
     },
@@ -55,13 +98,22 @@
       getTypeByLabel(index) {
         return Object.keys(this.$data.tabs)[index];
       },
-      handle(index) {
-        const jsonType = this.getTypeByLabel(index);
-        this.fetchJson({jsonType: jsonType});
+      fetch(index) {
+        const jsonType = this.$data.jsonType;
+        this.fetchJson({jsonType: jsonType, index: index});
+      },
+      handle(labelIndex) {
+        const jsonType = this.getTypeByLabel(labelIndex);
+        const index = this.$data.pagination.current[jsonType];
+        this.$data.jsonType = jsonType;
+        this.fetch(index);
+      },
+      pageChange(index) {
+        this.fetch(index);
       }
     },
     created() {
-      this.fetchJson({jsonType: 'contribution'});
+      this.fetch(1);
     }
   }
 </script>
@@ -69,6 +121,11 @@
 <style lang="scss" scoped>
   .subtitle {
     margin-bottom: 30px;
+  }
+
+  .pagination {
+    margin-top: 10px;
+    margin-bottom: 10px;
   }
 
   /deep/ {
