f8e32ef8f5235fe10d47e13e645cd933ec697f48
tmknom <tmk.nom@gmail.com>
2018-02-11 21:16:17 +0900

    記事ランキングで同評価の場合、ランキングを同率にするよう調整

--- app/application/content/article/QiitaArticleRankingApplication.scala
+++ app/application/content/article/QiitaArticleRankingApplication.scala
@@ -11,7 +11,9 @@ import spray.json._
 final class QiitaArticleRankingApplication @Inject()(
     repository: QiitaArticleAggregateRepository
 ) {
-  private val LIMIT = 100
+  private val LIMIT            = 100
+  private var lastContribution = -1
+  private var lastRank         = -1
 
   def create(): Unit = {
     createContribution()
@@ -22,26 +24,31 @@ final class QiitaArticleRankingApplication @Inject()(
   }
 
   private def createContribution(): Unit = {
+    init()
     val qiitaArticleAggregates = repository.retrieveContribution()
     createJsonFileAll("contribution", qiitaArticleAggregates)
   }
 
   private def createCommentsCount(): Unit = {
+    init()
     val qiitaArticleAggregates = repository.retrieveCommentsCount()
     createJsonFileAll("comment", qiitaArticleAggregates)
   }
 
   private def createHatenaCount(): Unit = {
+    init()
     val qiitaArticleAggregates = repository.retrieveHatenaCount()
     createJsonFileAll("hatena", qiitaArticleAggregates)
   }
 
   private def createFacebookCount(): Unit = {
+    init()
     val qiitaArticleAggregates = repository.retrieveFacebookCount()
     createJsonFileAll("facebook", qiitaArticleAggregates)
   }
 
   private def createPocketCount(): Unit = {
+    init()
     val qiitaArticleAggregates = repository.retrievePocketCount()
     createJsonFileAll("pocket", qiitaArticleAggregates)
   }
@@ -57,11 +64,33 @@ final class QiitaArticleRankingApplication @Inject()(
   private def createJsonFile(page: Int, offset: Int, fileType: String, qiitaArticleAggregates: Seq[QiitaArticleAggregate]): Unit = {
     val json = qiitaArticleAggregates.zipWithIndex.map {
       case (qiitaArticleAggregate, index) =>
-        QiitaArticleJson.build(qiitaArticleAggregate, offset + index + 1)
+        val rank = calculateRank(offset, index, fileType, qiitaArticleAggregate)
+        QiitaArticleJson.build(qiitaArticleAggregate, rank)
     }.toJson
 
     val fileName = s"/tmp/article.$fileType.${page.toString}.json"
     FileWriter.write(fileName, json)
+  }
+
+  private def calculateRank(offset: Int, index: Int, fileType: String, qiitaArticleAggregate: QiitaArticleAggregate): Int = {
+    val count = fileType match {
+      case "contribution" => qiitaArticleAggregate.contribution.likesCount.value
+      case "comment"      => qiitaArticleAggregate.contribution.commentsCount.value
+      case "hatena"       => qiitaArticleAggregate.contribution.hatenaCount.value
+      case "facebook"     => qiitaArticleAggregate.contribution.facebookCount.value
+      case "pocket"       => qiitaArticleAggregate.contribution.pocketCount.value
+    }
+
+    if (lastContribution != count) {
+      lastContribution = count
+      lastRank         = offset + index + 1
+    }
+    lastRank
+  }
+
+  private def init(): Unit = {
+    lastContribution = -1
+    lastRank         = -1
   }
 
   private def pageRange(max: Long): Range = {
