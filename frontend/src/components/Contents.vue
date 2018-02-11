ea716ed344960c009c3e55bd77979b8838a54bc7
tmknom <tmk.nom@gmail.com>
2018-02-11 16:16:56 +0900

    タブ切り替えの実装をbuefyで組み直し

--- frontend/src/components/Contents.vue
+++ frontend/src/components/Contents.vue
@@ -1,20 +1,47 @@
 <template>
-  <div id="contents">
-    <el-tabs type="card" @tab-click="handle">
-      <el-tab-pane v-bind:label="tabs.contribution">
+  <section id="contents" class="section">
+    <div class="container">
+      <section id="contents_header" class="section">
+        <!--https://qiita.com/hkusu/items/fda8d8178dd693f95f3c-->
+        <span class="fa-stack text-qiita">
+          <i class="fa fa-square fa-stack-2x"></i>
+          <i class="fa fa-stack-1x fa-inverse fa-search fa-2x"></i>
+        </span>
+        <span class="fa-stack text-twitter">
+          <i class="fas fa-square fa-stack-2x"></i>
+          <i class="fab fa-stack-1x fa-inverse fa-twitter"></i>
+          </span>
+        <span class="fa-stack text-facebook">
+          <i class="fas fa-square fa-stack-2x"></i>
+          <i class="fab fa-stack-1x fa-inverse fa-facebook"></i>
+        </span>
+        <span class="fa-stack text-get-pocket">
+          <i class="fas fa-square fa-stack-2x"></i>
+          <i class="fab fa-stack-1x fa-inverse fa-get-pocket"></i>
+        </span>
+
+        <h1 class="title">Section</h1>
+        <h2 class="subtitle">
+          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+        </h2>
+      </section>
+    </div>
+
+    <b-tabs type="is-boxed" @change="handle">
+      <b-tab-item :label="tabs.contribution">
         <ArticleList :articles="items"></ArticleList>
-      </el-tab-pane>
-      <el-tab-pane v-bind:label="tabs.hatena">
+      </b-tab-item>
+      <b-tab-item :label="tabs.hatena">
         <ArticleList :articles="items"></ArticleList>
-      </el-tab-pane>
-      <el-tab-pane v-bind:label="tabs.facebook">
+      </b-tab-item>
+      <b-tab-item :label="tabs.facebook">
         <ArticleList :articles="items"></ArticleList>
-      </el-tab-pane>
-      <el-tab-pane v-bind:label="tabs.pocket">
+      </b-tab-item>
+      <b-tab-item :label="tabs.pocket">
         <ArticleList :articles="items"></ArticleList>
-      </el-tab-pane>
-    </el-tabs>
-  </div>
+      </b-tab-item>
+    </b-tabs>
+  </section>
 </template>
 
 <script>
@@ -47,14 +74,11 @@
       ...mapActions('articles', [
         'fetchJson'
       ]),
-      getTypeByLabel(label) {
-        const tabs = this.$data.tabs;
-        return Object.keys(tabs).filter((key) => {
-          return tabs[key] === label
-        }).shift();
+      getTypeByLabel(index) {
+        return Object.keys(this.$data.tabs)[index];
       },
-      handle(tab, event) {
-        const jsonType = this.getTypeByLabel(tab.label);
+      handle(index) {
+        const jsonType = this.getTypeByLabel(index);
         this.fetchJson({jsonType: jsonType});
       }
     },
@@ -63,3 +87,21 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .text-qiita {
+    color: #4cb10d;
+  }
+
+  .text-twitter {
+    color: #55acee;
+  }
+
+  .text-facebook {
+    color: #3b5998;
+  }
+
+  .text-get-pocket {
+    color: #ee4256;
+  }
+</style>
