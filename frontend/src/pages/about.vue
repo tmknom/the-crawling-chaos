fa256368639501b551f99e58bd842f4cd9106adc
tmknom <tmk.nom@gmail.com>
2018-02-12 18:11:50 +0900

    Aboutページの実装

---
+++ frontend/src/pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <section id="contents" class="section">
+    <h1 class="title">About</h1>
+    <h2 class="subtitle">Qiitaをさまざまな角度から勝手に分析してみるサイトです。</h2>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+</style>
