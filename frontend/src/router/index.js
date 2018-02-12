08dee4e1b1b4ec78f36aabbed26764d9b1e0c8bb
tmknom <tmk.nom@gmail.com>
2018-02-12 18:46:56 +0900

    URLに#が入らないようにする＆エラーページの実装

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import index from '../pages/index.vue'
+import error from '../pages/error.vue'
 import about from '../pages/about.vue'
 
 Vue.use(VueRouter);
@@ -9,6 +10,10 @@ Vue.use(VueRouter);
 const routes = [
   {path: '/', component: index},
   {path: '/about', component: about},
+
+  // エラーページなので、必ず最後に記述する
+  // https://router.vuejs.org/ja/essentials/history-mode.html
+  {path: '*', component: error}
 ];
 
-export default new VueRouter({routes});
+export default new VueRouter({mode: 'history', routes: routes});
