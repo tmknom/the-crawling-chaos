c5da60f1bf186baf7e7ab48699c894c13f21cd0a
tmknom <tmk.nom@gmail.com>
2018-02-12 19:52:55 +0900

    ユーザランキング取得APIのモジュールを実装

---
+++ frontend/src/api/users.js
@@ -0,0 +1,20 @@
+// Ajax通信ライブラリ
+import axios from 'axios';
+
+export default {
+  request(jsonType, index) {
+    const request = async (_url) => {
+      const response = await axios.get(_url);
+      return response.data;
+    };
+    const url = this.getJsonUrl(jsonType, index);
+    return request(url);
+  },
+
+  getJsonUrl(index) {
+    const jsonType = 'article';
+    const path = '/qiita-ranker/user_contribution/user.' + jsonType + '.' + index + '.json.gz';
+    const baseUrl = 'http://temporary-7037dee17452.s3-website-ap-northeast-1.amazonaws.com';
+    return baseUrl + path;
+  }
+};
