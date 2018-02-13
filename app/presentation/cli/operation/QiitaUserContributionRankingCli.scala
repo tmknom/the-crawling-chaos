4f5db88e6dc6d4c1f09d2d49b9e185d5fde8cab1
tmknom <tmk.nom@gmail.com>
2018-02-13 22:10:41 +0900

    QiitaUserContributionRankingApplicationをQiitaUserRankingApplicationにリネーム

--- app/presentation/cli/operation/QiitaUserContributionRankingCli.scala
+++ app/presentation/cli/operation/QiitaUserContributionRankingCli.scala
@@ -1,6 +1,6 @@
 package presentation.cli.operation
 
-import application.content.QiitaUserContributionRankingApplication
+import application.content.QiitaUserRankingApplication
 import library.task.Task
 import play.api.{Application, Logger}
 
@@ -13,7 +13,7 @@ object QiitaUserContributionRankingCli extends App with Task {
   override def task(app: Application): Unit = {
     try {
       Logger.info(s"Started ${this.getClass.getSimpleName}.")
-      app.injector.instanceOf[QiitaUserContributionRankingApplication].create()
+      app.injector.instanceOf[QiitaUserRankingApplication].create()
       Logger.info(s"Completed ${this.getClass.getSimpleName}.")
     } catch {
       case e: Exception => Logger.error(s"Failed ${this.getClass.getSimpleName}.", e)
