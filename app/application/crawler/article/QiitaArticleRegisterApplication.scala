b2424d5f07e8d206195ece8f106b950c7f012e34
tmknom <tmk.nom@gmail.com>
2018-02-18 19:10:56 +0900

    QiitaArticleRegisterApplicationに登録失敗したユーザ名をログ出力するよう修正

--- app/application/crawler/article/QiitaArticleRegisterApplication.scala
+++ app/application/crawler/article/QiitaArticleRegisterApplication.scala
@@ -3,7 +3,9 @@ package application.crawler.article
 import javax.inject.{Inject, Singleton}
 
 import domain.crawler.Crawler
+import domain.qiita.article.json.RawPropsArticleJson
 import domain.qiita.article.{QiitaArticleIdRepository, QiitaArticleRepository, QiitaItemId, QiitaRawPropsArticleJsonRepository}
+import play.api.Logger
 
 @Singleton
 final class QiitaArticleRegisterApplication @Inject()(
@@ -15,14 +17,22 @@ final class QiitaArticleRegisterApplication @Inject()(
   def register(): Unit = {
     val items = idRepository.retrieveNotRegistered()
     withoutSleepLoop[QiitaItemId](items)(registerOne)
+    logNotRegisteredUserName()
   }
 
-  def registerOne(qiitaItemId: QiitaItemId): Unit = {
+  private def registerOne(qiitaItemId: QiitaItemId): Unit = {
     val optionValue = rawJsonRepository.retrieve(qiitaItemId)
     val rawArticleJson = optionValue match {
       case Some(v) => v
       case None    => throw new RuntimeException(s"ココに来たらバグなので雑に例外をスロー ${qiitaItemId.value}")
     }
     repository.register(rawArticleJson.toQiitaArticle)
+  }
+
+  private def logNotRegisteredUserName(): Unit = {
+    val qiitaItemIds = errors.map(QiitaItemId(_))
+    val RawPropsArticleJsons: Seq[RawPropsArticleJson] = rawJsonRepository.retrieveList(qiitaItemIds)
+    val postedUserNames:      Seq[String]              = RawPropsArticleJsons.map(_.toQiitaArticle.postedUserName.value).distinct
+    Logger.warn(s"${this.getClass.getSimpleName} error ${postedUserNames.size.toString} users : ${postedUserNames.toString()}")
   }
 }
