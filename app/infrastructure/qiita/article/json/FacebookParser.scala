9d7e6287516b7a5211a7fc9af3b4c77ebb9c3767
tmknom <tmk.nom@gmail.com>
2018-02-18 21:25:05 +0900

    FacebookParserがエラー時に0を返すよう修正

--- app/infrastructure/qiita/article/json/FacebookParser.scala
+++ app/infrastructure/qiita/article/json/FacebookParser.scala
@@ -4,9 +4,21 @@ import domain.qiita.article.contribution.FacebookCount
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.scraper.ContentExtractors._
+import play.api.Logger
 
 private[json] final case class FacebookParser(html: String) {
   def parse: FacebookCount = {
+    try {
+      parseThrowable
+    } catch {
+      case e: Exception => {
+        Logger.error(e.getMessage, e)
+        FacebookCount(0)
+      }
+    }
+  }
+
+  private def parseThrowable: FacebookCount = {
     val doc = JsoupBrowser().parseString(html)
     val raw = doc >> text("td._2pir #u_0_3 span")
 
