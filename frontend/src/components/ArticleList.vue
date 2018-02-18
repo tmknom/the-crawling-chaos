89dfb896acb0453fc4624e656e7f726bded9694e
tmknom <tmk.nom@gmail.com>
2018-02-18 15:12:19 +0900

    ArticleListのdivネストを削除

--- frontend/src/components/ArticleList.vue
+++ frontend/src/components/ArticleList.vue
@@ -1,42 +1,39 @@
 <template>
+  <b-table
+    :data="articles"
+    :bordered="false"
+    :striped="true"
+    :narrowed="false"
+    :hoverable="true"
+    :loading="false"
+    :mobile-cards="true">
 
-  <div id="articles">
-    <b-table
-      :data="articles"
-      :bordered="false"
-      :striped="true"
-      :narrowed="false"
-      :hoverable="true"
-      :loading="false"
-      :mobile-cards="true">
+    <template slot-scope="props">
+      <b-table-column field="index" label="順位" width="60" numeric>
+        {{ props.row.index }}
+      </b-table-column>
 
-      <template slot-scope="props">
-        <b-table-column field="index" label="順位" width="60" numeric>
-          {{ props.row.index }}
-        </b-table-column>
+      <b-table-column field="title" label="タイトル">
+        <a :href="props.row.article.url">{{ props.row.article.name }}</a>
+      </b-table-column>
 
-        <b-table-column field="title" label="タイトル">
-          <a :href="props.row.article.url">{{ props.row.article.name }}</a>
-        </b-table-column>
+      <b-table-column field="likes_count" label="いいね" width="70" numeric>
+        {{ props.row.contribution.likes_count.toLocaleString() }}
+      </b-table-column>
 
-        <b-table-column field="likes_count" label="いいね" width="70" numeric>
-          {{ props.row.contribution.likes_count.toLocaleString() }}
-        </b-table-column>
+      <b-table-column field="hatena_count" label="はてブ" width="70" numeric>
+        {{ props.row.contribution.hatena_count.toLocaleString() }}
+      </b-table-column>
 
-        <b-table-column field="hatena_count" label="はてブ" width="70" numeric>
-          {{ props.row.contribution.hatena_count.toLocaleString() }}
-        </b-table-column>
+      <b-table-column field="facebook_count" label="FB" width="70" numeric>
+        {{ props.row.contribution.facebook_count.toLocaleString() }}
+      </b-table-column>
 
-        <b-table-column field="facebook_count" label="FB" width="70" numeric>
-          {{ props.row.contribution.facebook_count.toLocaleString() }}
-        </b-table-column>
-
-        <b-table-column field="pocket_count" label="Pocket" width="70" numeric>
-          {{ props.row.contribution.pocket_count.toLocaleString() }}
-        </b-table-column>
-      </template>
-    </b-table>
-  </div>
+      <b-table-column field="pocket_count" label="Pocket" width="70" numeric>
+        {{ props.row.contribution.pocket_count.toLocaleString() }}
+      </b-table-column>
+    </template>
+  </b-table>
 </template>
 
 <script>
