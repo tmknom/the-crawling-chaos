08112ee96f8ea46eb564aabcf28de4e47e89774f
tmknom <tmk.nom@gmail.com>
2018-03-03 12:23:40 +0900

    QiitaUserContributionをフォーマット

--- app/domain/qiita/user/contribution/QiitaUserContribution.scala
+++ app/domain/qiita/user/contribution/QiitaUserContribution.scala
@@ -2,4 +2,10 @@ package domain.qiita.user.contribution
 
 import domain.qiita.article.contribution.HatenaCount
 
-final case class QiitaUserContribution(contribution: Contribution, articlesCount: ArticlesCount, hatenaCount: HatenaCount)
+final case class QiitaUserContribution(
+    contribution:  Contribution,
+    articlesCount: ArticlesCount,
+    hatenaCount:   HatenaCount
+) {
+
+}
