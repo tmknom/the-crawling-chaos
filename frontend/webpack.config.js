c7f37d843bb7384074126cc56d17eda4c5b5a58c
tmknom <tmk.nom@gmail.com>
2018-03-17 14:32:28 +0900

    webpack.config.jsからvarを駆逐

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
