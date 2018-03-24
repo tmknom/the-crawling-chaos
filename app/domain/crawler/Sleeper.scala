6b34cf72981e0f67b05bd096524760475b4a48f8
tmknom <tmk.nom@gmail.com>
2018-03-24 09:08:18 +0900

    スリープ時間を300ミリ秒に変更

--- app/domain/crawler/Sleeper.scala
+++ app/domain/crawler/Sleeper.scala
@@ -3,7 +3,7 @@ package domain.crawler
 import java.util.concurrent.TimeUnit
 
 object Sleeper {
-  private val SleepTimeMilliseconds = 100.toLong
+  private val SleepTimeMilliseconds = 300.toLong
 
   def sleep(): Unit = {
     TimeUnit.MILLISECONDS.sleep(SleepTimeMilliseconds)
