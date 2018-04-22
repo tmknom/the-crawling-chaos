79d08cbc33256911e6a6243269cb715994da4147
tmknom <tmk.nom@gmail.com>
2018-04-22 13:32:36 +0900

    s3_downloadタスクの削除

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -121,10 +121,3 @@ def s3_upload():
     '''S3へのアップロード'''
     import s3
     s3.upload()
-
-
-@task
-def s3_download():
-    '''S3へのダウンロード'''
-    import s3
-    s3.download()
--- fabfile/s3.py
+++ fabfile/s3.py
@@ -12,13 +12,6 @@ def upload():
     local("aws s3 sync %s s3://%s/qiita-ranker/" % (csv_path, s3_bucket))
 
 
-def download():
-    '''S3へのダウンロード'''
-    csv_path = get_local_env(PATH.CSV)
-    s3_bucket = get_local_env(AWS.S3_BUCKET)
-    local("aws s3 sync s3://%s/qiita-ranker/ %s" % (s3_bucket, csv_path))
-
-
 def deploy_json():
     '''S3へJSONをデプロイ'''
     upload_json('ranking')
